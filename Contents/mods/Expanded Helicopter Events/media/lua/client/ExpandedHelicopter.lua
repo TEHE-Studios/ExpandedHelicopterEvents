c6be1392e34927f59220b7c01fd1949b9e9c4d37
ChuckTheJittai <ChuckTheSheep@users.noreply.github.com>
2021-03-17 13:01:36 -0400

    Update ExpandedHelicopter.lua

--- Contents/mods/Expanded Helicopter Events/media/lua/client/ExpandedHelicopter.lua
+++ Contents/mods/Expanded Helicopter Events/media/lua/client/ExpandedHelicopter.lua
@@ -4,12 +4,15 @@
 ---@field currentPosition Vector3
 ---@field speed number
 ---@field emitter FMODSoundEmitter
+---@field ID number
 
 eHelicopter = {}
-eHelicopter.targetPos = {}--Vector3.new()
-eHelicopter.movement = {}--Vector3.new()
-eHelicopter.currentPosition = {}--Vector3.new()
+eHelicopter.targetPos = Vector3.new()
+eHelicopter.movement = Vector3.new()
+eHelicopter.currentPosition = Vector3.new()
 eHelicopter.speed = 20
+eHelicopter.height = 20
+eHelicopter.ID = 0
 
 function eHelicopter:new()
 
@@ -22,13 +25,37 @@ MAX_XY = 15000
 MIN_XY = 0
 
 
+--- This is the equivalent of a getter for Vector3---
+function smashShmector(pointToString)
+	--example: java.awt.Point[x=12345,y=12345]
+	pointToString = string.gsub(pointToString, "java.awt.Point%[x=", "")
+	pointToString = string.gsub(pointToString, "y=", "")
+	pointToString = string.gsub(pointToString, "%]", "")
+	return pointToString
+end
+
+---@param ShmectorTree Vector3
+---@return number x of ShmectorTree
+function Vector3GetX(ShmectorTree)
+	local point = smashShmector(tostring(ShmectorTree:toAwtPoint()))
+	local x,_ = point:match("^(.+),(.+)$")
+	return tonumber(x)
+end
+
+---@param ShmectorTree Vector3
+---@return number y of ShmectorTree
+function Vector3GetY(ShmectorTree)
+	local point = smashShmector(tostring(ShmectorTree:toAwtPoint()))
+	local _,y = point:match("^(.+),(.+)$")
+	return tonumber(y)
+end
+
+
 function eHelicopter:initPos()
 	---@type float
 	local initX = 0
 	---@type float
 	local initY = 0
-	---@type float
-	local initZ = 20
 
 	if ZombRand(101) > 50 then --50/50
 		initX = MAX_XY
@@ -44,66 +71,41 @@ function eHelicopter:initPos()
 		end
 	end
 
-	self.currentPosition = Vector3.new(initX, initY, initZ)
-	print("initPos: currentPosition: "..tostring(self.currentPosition))
-
-	print("initPos: currentPosition: xyz"..tostring(self.currentPosition.x)..","..tostring(self.currentPosition.y)..","..tostring(self.currentPosition.z))
+	self.currentPosition:set(initX, initY, self.height)
+	print("initPos: currentPosition: xy +z  "..Vector3GetX(self.currentPosition)..","..Vector3GetX(self.currentPosition)..","..self.height)
 
 end
 
 
 function eHelicopter:isInBounds()
 
-	if ( self.currentPosition.x <= MAX_XY and
-			self.currentPosition.x >= MIN_XY and
-			self.currentPosition.y <= MAX_XY and
-			self.currentPosition.y >= MIN_XY ) then
+	if Vector3GetX(self.currentPosition) <= MAX_XY and Vector3GetX(self.currentPosition) >= MIN_XY and Vector3GetY(self.currentPosition) <= MAX_XY and Vector3GetY(self.currentPosition) >= MIN_XY then
 		return true
 	end
+
 	return false
 end
 
 
-function eHelicopter:unlaunch()
-
-	Events.OnTick.Remove(self.update)
-	self.emitter.stopAll()
-end
-
-
-function eHelicopter:update()
-
-	self:moveToPosition(self.targetPos, true)
-
-	if not self:isInBounds() then
-		self:unlaunch()
-	end
-end
-
-
-
-
 ---Rewriting Vector3.aimAt to use Vector3 rather than Vector2
----@param targetPosition Vector3
+---@param newTargetPos Vector3
 ---@return Vector3
-function eHelicopter:ehe_aimAt(targetPosition)
+function eHelicopter:ehe_aimAt(newTargetPos)
 
 	print("applyMovement: currentPosition: "..tostring(self.currentPosition))
 	print("applyMovement: movement: "..tostring(self.movement))
-	print("applyMovement: targetPosition: "..tostring(targetPosition))
 	print("applyMovement: targetPos: "..tostring(self.targetPos))
+	print("applyMovement: newTargetPos: "..tostring(newTargetPos))
 
-	---@type Vector3 movementVector3
-	local movementVector3 = Vector3.new()
 	---@type float
-	local direction = math.atan(targetPosition.y - self.currentPosition.y, targetPosition.x - self.currentPosition.x)
+	local direction = math.atan(Vector3GetY(newTargetPos) - Vector3GetY(self.currentPosition), Vector3GetX(newTargetPos) - Vector3GetX(self.currentPosition))
 	---@type float
-	local length = math.sqrt(self.currentPosition.x * self.currentPosition.x + self.currentPosition.y * self.currentPosition.y)
+	local length = math.sqrt(Vector3GetX(self.currentPosition) * Vector3GetX(self.currentPosition) + Vector3GetY(self.currentPosition) * Vector3GetY(self.currentPosition))
 
-	movementVector3:setLengthAndDirection(direction, length)
-	movementVector3.z = self.currentPosition.z
+	local new_x = math.cos(direction) * length
+	local new_y = math.sin(direction) * length
 
-	return movementVector3
+	return Vector3.new(new_x, new_y, self.height)
 end
 
 
@@ -114,7 +116,7 @@ function eHelicopter:aimAtTarget()
 
 	print("aimAtTarget: aimedVector3: "..tostring(aimedVector3))
 
-	self.movement = Vector3.new(aimedVector3)
+	self.movement:set(aimedVector3)
 
 	print("aimAtTarget: currentPosition: "..tostring(self.currentPosition))
 	print("aimAtTarget: targetPos: "..tostring(self.targetPos))
@@ -135,23 +137,22 @@ function eHelicopter:applyMovement(dampen)
 		self:dampenMovement(movementVector3)
 	end
 
-	self.currentPosition.x = self.currentPosition.x + movementVector3.x
-	self.currentPosition.y = self.currentPosition.y + movementVector3.y
+	self.currentPosition:set(Vector3GetX(self.currentPosition) + Vector3GetX(movementVector3), Vector3GetY(self.currentPosition) + Vector3GetY(movementVector3), self.height)
 
 	print("applyMovement: currentPosition: "..tostring(self.currentPosition))
 	print("applyMovement: movement: "..tostring(self.movement))
 	print("applyMovement: targetPos: "..tostring(self.targetPos))
 
-	self.emitter:setPos(self.currentPosition.x,self.currentPosition.y,self.currentPosition.z)
+	self.emitter:setPos(Vector3GetX(self.currentPosition),Vector3GetY(self.currentPosition),self.height)
 end
 
 
 function eHelicopter:dampenMovement()
 
 	self.movement:set(
-	--[[x]](movement.x * math.max(0.1,((self.targetPos.x - self.currentPosition.x) / self.targetPos.x))),
-	--[[y]](movement.y * math.max(0.1,((self.targetPos.y - self.currentPosition.y) / self.targetPos.y))),
-	--[[z]](self.currentPosition.z)
+	--[[x]](Vector3GetX(self.movement) * math.max(0.1,((Vector3GetX(self.targetPos) - Vector3GetX(self.currentPosition)) / Vector3GetX(self.targetPos)))),
+	--[[y]](Vector3GetY(self.movement) * math.max(0.1,((Vector3GetY(self.targetPos) - Vector3GetY(self.currentPosition)) / Vector3GetY(self.targetPos)))),
+	--[[z]](self.height)
 	)
 end
 
@@ -176,33 +177,48 @@ function eHelicopter:launch(targetedPlayer)
 	if not targetedPlayer then
 		--the -1 is to offset playerIDs starting at 0
 		local numActivePlayers = getNumActivePlayers()-1
-		print("numActivePlayers: "..numActivePlayers)
 		local randNumFromActivePlayers = ZombRand(numActivePlayers)
-		print("randNumFromActivePlayers: "..randNumFromActivePlayers)
 		targetedPlayer = getSpecificPlayer(randNumFromActivePlayers)
 	end
 
-	if not targetedPlayer then
-		print("no targetedPlayer")
-		return
-	end
-	
-	print("launch: currentPosition: "..tostring(self.currentPosition))
-
-	print("launch: target:"..targetedPlayer:getDescriptor():getSurname().."  (x: "..targetedPlayer:getX().." y: "..targetedPlayer:getY()..")")
-
-	self.targetPos = Vector3.new(targetedPlayer:getX(),targetedPlayer:getY(),self.currentPosition.z)
-
+	self.targetPos:set(targetedPlayer:getX(),targetedPlayer:getY(),self.height)
 	self:aimAtTarget()
 
-	print("launch: targetPos: "..tostring(self.targetPos))
+	self.emitter = getWorld():getFreeEmitter(Vector3GetX(self.currentPosition), Vector3GetY(self.currentPosition), self.height)
+	self.emitter:playSound("Helicopter", getSquare(Vector3GetX(self.currentPosition), Vector3GetY(self.currentPosition), self.height))
 
-	self.emitter = getWorld():getFreeEmitter(self.currentPosition.x, self.currentPosition.y, self.currentPosition.z)
-	if not self.emitter then print("no emitter found") end
-	--self.emitter:playSound("Helicopter", getSquare(self.currentPosition.x, self.currentPosition.y, self.currentPosition.z))
-
-	Events.OnTick.Add(self.update)
+	table.insert(ALL_HELICOPTERS, self)
+	self.ID = #ALL_HELICOPTERS
 end
+
+
+ALL_HELICOPTERS = {}
+
+function updateAllHelicopters()
+	for key,_ in ipairs(ALL_HELICOPTERS) do
+		---@type eHelicopter heli
+		local heli = ALL_HELICOPTERS[key]
+		heli:update()
+	end
+end
+
+
+function eHelicopter:unlaunch()
+	ALL_HELICOPTERS[self.ID] = nil
+	self.emitter.stopAll()
+end
+
+
+function eHelicopter:update()
+
+	self:moveToPosition(self.targetPos, true)
+
+	if not self:isInBounds() then
+		self:unlaunch()
+	end
+end
+
+Events.OnTick.Add(updateAllHelicopters)
 
 
 --- Used only for testing purposes
@@ -214,12 +230,5 @@ if key == Keyboard.KEY_0 then
 	local heli = eHelicopter:new()
 	heli:launch()
 
-	--elseif key == Keyboard.KEY_9 then---add different behaviors + send away
-	--	if eHelicopter.emitter then
-	--		eHelicopter.emitter.stopAll()
-	--		print("Stopping helicopter emitter")
-	--	else
-	--		print("Unable to find helicopter emitter!")
-	--	end
 	end
 end)
