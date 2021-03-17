bb6acf2ee8df92cacdf2749f08a9235a5597601a
ChuckTheJittai <ChuckTheSheep@users.noreply.github.com>
2021-03-16 20:26:08 -0400

    Version 0.0

--- Contents/mods/Expanded Helicopter Events/media/lua/client/ExpandedHelicopter.lua
+++ Contents/mods/Expanded Helicopter Events/media/lua/client/ExpandedHelicopter.lua
@@ -1,146 +1,186 @@
 ---@class eHelicopter
----@field soundRef long
 ---@field emitter FMODSoundEmitter
----@field pos position
----@field target _Vector2
----@field movement _Vector2
 eHelicopter = {}
-eHelicopter.target = { [x]=0, [y]=0 }
-eHelicopter.movement = { [x]=0, [y]=0 }
+eHelicopter.MAX_XY = 15000
+eHelicopter.MIN_XY = 0
 
----@class position
----@field x float
----@field y float
----@field z float
-eHelicopter.pos = { [x]=0, [y]=0, [z]=20 }
+---@field targetPos Vector3
+---@field movement Vector3 consider this as a kind of velocity (direction and speed)
+eHelicopter.targetPos = Vector3.new()
+eHelicopter.movement = Vector3.new()
+eHelicopter.currentPosition = Vector3.new()
 
----@param target IsoMovingObject
----@return long @sound reference number
-function eHelicopter.playSound()
-	eHelicopter.emitter = getWorld().getFreeEmitter(pos.x, pos.y, pos.z)
-	ModLogger.debug("Created new helicopter emitter (" .. tostring(result) .. ')')
-
-	return eHelicopter.emitter.playSoundImpl("Helicopter", self)
-end
-
-
----@return boolean
-function eHelicopter.isSoundPlaying() --- may not need
-	return eHelicopter.emitter and eHelicopter.emitter.isPlaying("Helicopter") or false
-end
+eHelicopter.speed = 20
 
 
 function eHelicopter.initPos()
-	---@type position
-	local pos = eHelicopter.pos
-	--places the helicopter on the "edge" of the map (edge being -15k to 15k)
-	--50/50 chance for which axis is randomized entirely
-	--followed by 50/50 whether the static axis is positive or negative
-	--- there might be a more elegant mathematical implementation for this
-	if ZombRand(101) > 50 then
-		pos.y = ZombRand(0,15000)
-		pos.x = 15000
-		if ZombRand(101) > 50 then
-			pos.x = 0
+
+	local initX = 0
+	local initY = 0
+	local initZ = 20
+
+	if ZombRand(101) > 50 then --50/50
+		initX = MAX_XY
+		initY = ZombRand(MIN_XY,MAX_XY)
+		if ZombRand(101) > 50 then --50/50
+			initX = MIN_XY
 		end
 	else
-		pos.x = ZombRand(0,15000)
-		pos.y = 15000
-		if ZombRand(101) > 50 then
-			pos.y = 0
+		initX = ZombRand(MIN_XY,MAX_XY)
+		initY = MAX_XY
+		if ZombRand(101) > 50 then --50/50
+			initY = MIN_XY
 		end
 	end
-	pos.z = 20
+
+	eHelicopter.currentPosition:set(initX, initY, initZ)
 end
 
 
 ---@param targetedPlayer IsoMovingObject | IsoPlayer | IsoGameCharacter @ random player if blank
 function eHelicopter.launch(targetedPlayer)
 
+	eHelicopter.initPos()
+
 	if not targetedPlayer then
 		--the -1 is to offset playerIDs starting at 0
 		local numActivePlayers = getNumActivePlayers()-1
-		print("numActivePlayers:"..numActivePlayers)
+		print("numActivePlayers: "..numActivePlayers)
 		local randNumFromActivePlayers = ZombRand(numActivePlayers)
-		print("randNumFromActivePlayers:"..randNumFromActivePlayers)
+		print("randNumFromActivePlayers: "..randNumFromActivePlayers)
 		targetedPlayer = getSpecificPlayer(randNumFromActivePlayers)
 	end
 
-	print("targetedPlayer: "..targetedPlayer:getDescriptor():getForename().." "..targetedPlayer:getDescriptor():getSurname().." (pos:"..targetedPlayer:getX()..","..targetedPlayer:getY())
+	if not targetedPlayer then
+		print("no targetedPlayer")
+		return
+	end
 
-	eHelicopter.target:set(targetedPlayer:getX(),targetedPlayer:getY())
-	--ModLogger.debug("Set helicopter target to player " .. target.getObjectName())
+	eHelicopter.aimAtTarget()
 
-	eHelicopter.initPos()
+	print("targetedPlayer: "..targetedPlayer:getDescriptor():getForename().." "..targetedPlayer:getDescriptor()
+			:getSurname().." (pos:"..targetedPlayer:getX()..","..targetedPlayer:getY())
 
-	--start playing helicopter sound
-	local ref = eHelicopter.playSound()
-	ModLogger.debug("Playing helicopter noise (" .. tostring(ref) .. ')')
-	eHelicopter.soundRef = ref
-	---not sure if ref is useful for anything
+	eHelicopter.targetPos:set(targetedPlayer:getX(),targetedPlayer:getY(),eHelicopter.currentPosition.z)
 
-	eHelicopter.setUpMovement(eHelicopter.target)
+	eHelicopter.emitter = getWorld():getFreeEmitter(eHelicopter.currentPosition.x, eHelicopter.currentPosition.y, eHelicopter.currentPosition.z)
+	if not eHelicopter.emitter then print("no emitter found") end
+	--eHelicopter.emitter:playSound("Helicopter", getSquare(eHelicopter.currentPosition.x, eHelicopter.currentPosition.y, eHelicopter.currentPosition.z))
 
 	Events.OnTick.Add(eHelicopter.update)
 end
 
 
+function eHelicopter.isInBounds()
+
+	if ( eHelicopter.currentPosition.x <= MAX_XY and
+			eHelicopter.currentPosition.x >= MIN_XY and
+			eHelicopter.currentPosition.y <= MAX_XY and
+			eHelicopter.currentPosition.y >= MIN_XY ) then
+		return true
+	end
+	return false
+end
+
+
 function eHelicopter.update()
-	eHelicopter.moveStep(eHelicopter.movement)
-	if ( helicopter.pos.x < 15000 and helicopter.pos.y < 15000 ) then
+
+	eHelicopter.moveToPosition(eHelicopter.targetPos, true)
+
+	if not eHelicopter.isInBounds() then
 		eHelicopter.unlaunch()
 	end
 end
 
+
 function eHelicopter.unlaunch()
+
 	Events.OnTick.Remove(eHelicopter.update)
 	eHelicopter.emitter.stopAll()
 end
 
----@param destination _Vector2
-function eHelicopter.setUpMovement(destination)
 
-	eHelicopter.movement:set(eHelicopter.pos.x,eHelicopter.pos.y)
+---Rewriting Vector3.aimAt to use Vector3
+---@param currentPos Vector3
+---@param target Vector3
+---@return Vector3
+function eHelicopter.ehe_aimAt(currentPos,target)
 
-	movement:aimAt(destination)
-	movement:normalize()
-	movement:setLength(speed)
+	---@type Vector3 movement
+	local movementVector3 = Vector3.new()
 
-	return movement
+	local direction = Math:atan2((target.y - currentPos.y), (target.x - currentPos.x))
+	local length = Math:sqrt(currentPos.x * currentPos.x + currentPos.y * currentPos.y)
+
+	movementVector3:setLengthAndDirection(direction, length)
+	movementVector3.z = eHelicopter.currentPosition.z
+
+	return movementVector3
 end
 
 
----@param movement _Vector2
----@param destination _Vector2
-function eHelicopter.moveStep(movement)
+function eHelicopter.aimAtTarget()
 
-	eHelicopter.pos.x = eHelicopter.pos.x+movement.x
-	eHelicopter.pos.y = eHelicopter.pos.y+movement.y
+	print("eHelicopter.currentPosition: "..tostring(eHelicopter.currentPosition))
+	print("eHelicopter.targetPos: "..tostring(eHelicopter.targetPos))
 
-	print("HELI: X:"..eHelicopter.pos.x.."  Y:"..eHelicopter.pos.y)
+	eHelicopter.movement:set(eHelicopter.ehe_aimAt(eHelicopter.currentPosition,eHelicopter.targetPos))
 
-	eHelicopter.emitter.setPos(eHelicopter.pos.x,eHelicopter.pos.y,eHelicopter.pos.z)
+	print("eHelicopter.movement: "..tostring(eHelicopter.movement))
+
+	eHelicopter.movement:normalize()
+	eHelicopter.movement:setLength(eHelicopter.speed)
 end
 
 
----@param movement _Vector2
----@param destination _Vector2
-function eHelicopter.moveDampen(movement, destination)
-	movement.x = movement.x * math.max(0.1,((destination.x - position.x)/destination.x))
-	movement.y = movement.y * math.max(0.1,((destination.y - position.y)/destination.y))
+---@param dampen boolean
+function eHelicopter.applyMovement(dampen)
+
+	---@type Vector3 movement
+	local movementVector3 = eHelicopter.movement:clone()
+
+	if dampen then
+		eHelicopter.dampenMovement(movementVector3)
+	end
+
+	eHelicopter.currentPosition:add(movementVector3)
+
+	print("HELI: X:"..eHelicopter.currentPosition.x .."  Y:"..eHelicopter.currentPosition.y)
+
+	eHelicopter.emitter:setPos(eHelicopter.currentPosition.x,eHelicopter.currentPosition.y,eHelicopter.currentPosition.z)
+end
+
+
+---@param movement Vector3
+function eHelicopter.dampenMovement(movement)
+
+	--movement:set((movement.x * math.max(0.1,((eHelicopter.targetPos.x - eHelicopter.currentPosition.x) / eHelicopter.targetPos.x))), (movement.y * math.max(0.1,((eHelicopter.targetPos.y - eHelicopter.currentPosition.y) / eHelicopter.targetPos.y))), (eHelicopter.currentPosition.z))
+end
+
+
+---@param destination Vector3
+---@param dampen boolean
+function eHelicopter.moveToPosition(destination, dampen)
+
+	if destination then
+		eHelicopter.aimAtTarget(destination)
+	end
+
+	eHelicopter.applyMovement(dampen)
 end
 
 
 --- Used only for testing purposes
 Events.OnCustomUIKey.Add(function(key)
-	if key == Keyboard.KEY_0 then
-		eHelicopter.launch()
+if key == Keyboard.KEY_0 then
+	print("0 key pressed :')")
+	eHelicopter.launch()
 	--elseif key == Keyboard.KEY_9 then---add different behaviors + send away
 	--	if eHelicopter.emitter then
 	--		eHelicopter.emitter.stopAll()
-	--		ModLogger.debug("Stopping helicopter emitter")
+	--		print("Stopping helicopter emitter")
 	--	else
-	--		ModLogger.error("Unable to find helicopter emitter!")
+	--		print("Unable to find helicopter emitter!")
 	--	end
 	end
 end)
