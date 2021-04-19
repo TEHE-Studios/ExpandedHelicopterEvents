9a247497780b5e86ac27c81a750c5195ebd94117
Chuckleberry Finn <6071161+ChuckTheSheep@users.noreply.github.com>
2021-04-19 09:47:01 -0400

    Improves documentation

--- Contents/mods/Expanded Helicopter Events/media/lua/client/ExpandedHelicopter05_EventScheduler.lua
+++ Contents/mods/Expanded Helicopter Events/media/lua/client/ExpandedHelicopter05_EventScheduler.lua
@@ -1,23 +1,33 @@
+---Inserts a new eHeliEvent (table) to the "EventsSchedule" table
+---@param replacePos number If not nil, replace specific entry in "EventsSchedule", otherwise insert as normal (at end)
+---@param startDay number Day scheduled for start of this event
+---@param startTime number Hour scheduled for the start of this event
+---@param endTime number Hour scheduled for the end of this event
+---@param renew boolean If the event reschedules after completion
+---Events are handled as tables because ModData does not save Lua "classes" properly, even though they are really tables.
 function eHeliEvent_new(replacePos, startDay, startTime, endTime, renew)
-
+	
 	if (not startDay) or (not startTime) or (not endTime) then
 		return
 	end
 
-	local o = {["startDay"] = startDay, ["startTime"] = startTime, ["endTime"] = endTime, ["renew"] = renew, ["triggered"] = false}
+	local newEvent = {["startDay"] = startDay, ["startTime"] = startTime, ["endTime"] = endTime, ["renew"] = renew, ["triggered"] = false}
 
 	if replacePos then
-		getGameTime():getModData()["EventsSchedule"][replacePos] = o
+		getGameTime():getModData()["EventsSchedule"][replacePos] = newEvent
 	else
-		table.insert(getGameTime():getModData()["EventsSchedule"], o)
+		table.insert(getGameTime():getModData()["EventsSchedule"], newEvent)
 	end
 end
 
-
+---Calculates if a flight should go out and the weather impact on flight safety
+---@return boolean, number returns two values: willFly, impactOnFlightSafety
 function eHeliEvent_weatherImpact()
 	local CM = getClimateManager()
+	
 	local willFly = true
 	local impactOnFlightSafety = 0
+	
 	local wind = CM:getWindIntensity()
 	local fog = CM:getFogIntensity()
 	local rain = CM:getRainIntensity()/2
@@ -33,77 +43,32 @@ function eHeliEvent_weatherImpact()
 	return willFly, impactOnFlightSafety
 end
 
-
+--Engages specific eHeliEvent based on ID
+---@param ID number position in "EventsSchedule"
 function eHeliEvent_engage(ID)
+	--if frequency is set to never
 	if eHelicopterSandbox.config.frequency == 0 then
 		return
 	end
 
 	local eHeliEvent = getGameTime():getModData()["EventsSchedule"][ID]
 	eHeliEvent.triggered = true
-
+	
+	--check if the event will occur
 	local willFly,_ = eHeliEvent_weatherImpact()
-
 	if willFly then
 		getFreeHelicopter():launch()
 	end
-
+	--replace event in schedule with newly generated values
 	if eHeliEvent.renew then
 		setNextHeliFrom(ID)
 	end
 end
 
-
+---General events cut-off day after apocalypse, NOT game start
 eHeliEvent_cutOffDay = 30
-function setNextHeliFrom(ID, heliDay, heliStart, heliEnd)
-
-	if eHelicopterSandbox.config.frequency == 0 then
-		return
-	end
-
-	local lastHeliEvent = getGameTime():getModData()["EventsSchedule"][ID]
-
-	if not heliDay then
-
-		local nightsSurvived = getGameTime():getNightsSurvived()
-
-		if lastHeliEvent then
-			heliDay = lastHeliEvent.startDay
-		else
-			heliDay = nightsSurvived
-		end
-
-		-- options = Never=0, Once=1, Sometimes=2, Often=3
-		if eHelicopterSandbox.config.frequency <= 2 then
-			heliDay = heliDay+ZombRand(3, 6)
-			-- if frequency is 3 / often
-		elseif eHelicopterSandbox.config.frequency == 3 then
-			heliDay = heliDay+ZombRand(1, 2)
-		end
-		
-		heliDay = heliDay+math.floor(7*(nightsSurvived/eHeliEvent_cutOffDay))
-	end
-
-	if not heliStart then
-		--start time is random from hour 9 to 19
-		heliStart = ZombRand(9, 19)
-	end
-
-	if not heliEnd then
-		--end time is start time + 1 to 5 hours
-		heliEnd = heliStart+ZombRand(1,5)
-	end
-
-	local renewHeli = true
-	if (eHelicopterSandbox.config.frequency == 1) or (eHeli_getDaysBeforeApoc+heliDay > eHeliEvent_cutOffDay) then
-		renewHeli = false
-	end
-
-	eHeliEvent_new(ID, heliDay, heliStart, heliEnd, renewHeli)
-end
-
-
----Check how many days it has been since the start of the apocalypse
+---Check how many days it has been since the start of the apocalypse; corrects for sandbox option "Months since Apoc"
+---@return number Days since start of in-game apocalypse
 function eHeli_getDaysBeforeApoc()
 
 	local monthsAfterApo = getSandboxOptions():getTimeSinceApo()-1
@@ -139,7 +104,63 @@ function eHeli_getDaysBeforeApoc()
 	return apocDays
 end
 
+---Generates a schedule time for an event, either from scratch or a previous event.
+---@param ID number Position in schedule
+---@param heliDay number Day to start event
+---@param heliStart number Hour to start event
+---@param heliEnd number Hour to end event
+function setNextHeliFrom(ID, heliDay, heliStart, heliEnd)
+	
+	local freq = eHelicopterSandbox.config.frequency
+	--if freq is never
+	if freq == 0 then
+		return
+	end
+	--grab old event based on ID
+	local lastHeliEvent = getGameTime():getModData()["EventsSchedule"][ID]
 
+	if not heliDay then
+	
+		local nightsSurvived = getGameTime():getNightsSurvived()
+		--if there is an old event use it's start day for calculating the next start day
+		if lastHeliEvent then
+			heliDay = lastHeliEvent.startDay
+		--otherwise use nightsSurvived / current day
+		else
+			heliDay = nightsSurvived
+		end
+
+		--options = Never=0, Once=1, Sometimes=2, Often=3
+		if freq <= 2 then
+			heliDay = heliDay+ZombRand(3, 6)
+		--if frequency is often
+		elseif freq == 3 then
+			heliDay = heliDay+ZombRand(1, 2)
+		end
+		--as days get closer to the cutoff the time between new events gets longer
+		heliDay = heliDay+math.floor((7-freq)*(nightsSurvived/eHeliEvent_cutOffDay))
+	end
+
+	if not heliStart then
+		--start time is random from hour 9 to 19
+		heliStart = ZombRand(9, 19)
+	end
+
+	if not heliEnd then
+		--end time is start time + 1 to 5 hours
+		heliEnd = heliStart+ZombRand(1,5)
+	end
+
+	local renewHeli = true
+	--if freq is once OR new heliDay is beyond cutOffDay don't renew further
+	if (freq == 1) or (eHeli_getDaysBeforeApoc+heliDay > eHeliEvent_cutOffDay) then
+		renewHeli = false
+	end
+
+	eHeliEvent_new(ID, heliDay, heliStart, heliEnd, renewHeli)
+end
+
+---Handles setting up the event scheduler
 function eHeliEvents_OnGameStart()
 
 	--if no ModData found make it an empty list
@@ -166,7 +187,7 @@ end
 
 Events.OnGameStart.Add(eHeliEvents_OnGameStart)
 
-
+--Checks every hour if there is an event scheduled to engage
 function eHeliEvent_Loop()
 	local DAY = getGameTime():getNightsSurvived()
 	local HOUR = getGameTime():getHour()
