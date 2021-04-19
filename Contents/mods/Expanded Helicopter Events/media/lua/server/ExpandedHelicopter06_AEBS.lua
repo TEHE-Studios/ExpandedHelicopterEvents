ae822b1174a5a72d7849246bc2b1b56a28767734
Chuckleberry Finn <6071161+ChuckTheSheep@users.noreply.github.com>
2021-04-19 08:48:03 -0400

    Improved documentation

--- Contents/mods/Expanded Helicopter Events/media/lua/server/ExpandedHelicopter06_AEBS.lua
+++ Contents/mods/Expanded Helicopter Events/media/lua/server/ExpandedHelicopter06_AEBS.lua
@@ -1,4 +1,4 @@
----stores and adds on to the functions in /media/lua/server/radio/ISWeatherChannel.lua
+---stores and adds on to functions found in /media/lua/server/radio/ISWeatherChannel.lua
 local WeatherChannel_FillBroadcast = WeatherChannel.FillBroadcast
 
 --local function from ISWeatherChannel.lua
@@ -9,11 +9,11 @@ local function comp(_str)
 end
 
 function WeatherChannel.FillBroadcast(_gametime, _bc)
-
+	--call stored version from above using the same arguments
 	WeatherChannel_FillBroadcast(_gametime, _bc)
-
+	
 	local c = { r=1.0, g=1.0, b=1.0 };
-
+	--check if flights would be prevented due to weather
 	local willFly,_ = eHeliEvent_weatherImpact()
 	if willFly then
 		for _,v in pairs(getGameTime():getModData()["EventsSchedule"]) do
