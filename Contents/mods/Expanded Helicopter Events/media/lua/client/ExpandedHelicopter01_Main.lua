8314d058c366e1ce651f4ba51f18890470ba6596
ChuckTheJittai <ChuckTheSheep@users.noreply.github.com>
2021-04-23 17:34:33 -0400

    Remove preset functions from _Main

--- Contents/mods/Expanded Helicopter Events/media/lua/client/ExpandedHelicopter01_Main.lua
+++ Contents/mods/Expanded Helicopter Events/media/lua/client/ExpandedHelicopter01_Main.lua
@@ -6,6 +6,9 @@ ALL_HELICOPTERS = {}
 ---@class eHelicopter
 eHelicopter = {}
 
+---@field subEvents table
+eHelicopter.subEvents = {}
+
 ---@field randomEdgeStart boolean
 eHelicopter.randomEdgeStart = false
 
@@ -106,63 +109,6 @@ eHelicopter.lastAttackTime = -1
 eHelicopter.hostilesToFireOnIndex = 0
 ---@field hostilesToFireOn table
 eHelicopter.hostilesToFireOn = {}
-
-
----Preset list, only include variables being changed.
-eHelicopter_PRESETS = {
-	["increasingly_hostile"] = {presetProgression = {["patrol_only"] = 0, ["attack_only_undead"] = 0.15, ["attack_only_all"] = 0.75}},
-
-	["jet"] = {randomEdgeStart = true, frequencyFactor = 0.33, speed = 3, flightVolume = 25, flightSound = "eJetFlight", hostilePreference = false, announcerVoice = false},
-	["news_chopper"] = {frequencyFactor = 2, speed = 0.2, topSpeedFactor = 5, hostilePreference = false, announcerVoice = false, cutOffDay = 15},
-	["patrol_only"] = {hostilePreference = false},
-	["attack_only_undead"] = {announcerVoice = false},
-	["attack_only_all"] = {announcerVoice = false, hostilePreference = nil},
-}
-
-
----@param ID string
-function eHelicopter:loadPreset(ID)
-
-	if not ID then
-		return
-	end
-
-	local preset = eHelicopter_PRESETS[ID]
-
-	if not preset then
-		return
-	end
-
-	local pp = preset.presetProgression
-	if pp then
-		local DaysSinceApoc = getGameTime():getModData()["DaysBeforeApoc"]+getGameTime():getNightsSurvived()
-		local CutOff = preset.cutOffDay or eHelicopter.cutOffDay
-		local DaysOverCutOff = DaysSinceApoc/CutOff
-		local presetIDTmp
-		--run through presetProgression list
-		for pID,pCutOff in pairs(pp) do
-			--if progression % is less than DaysOverCutOff
-			if pCutOff <= DaysOverCutOff then
-				--if there is no stored % or progression % > stored %
-				if (not presetIDTmp) or (presetIDTmp and (pCutOff > pp[presetIDTmp])) then
-					--store qualifying choice
-					presetIDTmp = pID
-				end
-			end
-		end
-		if presetIDTmp then
-			--replace original preset with qualifying preset
-			preset = eHelicopter_PRESETS[presetIDTmp]
-		end
-	end
-
-	--local reportPreset = "loading preset: "..ID.."  vars:"
-	for var,value in pairs(preset) do
-		--reportPreset = reportPreset.." -"..var.." = "..tostring(value)
-		self[var] = value
-	end
-	--print(reportPreset)
-end
 
 
 ---Do not call this function directly for new helicopters; use: getFreeHelicopter instead
