e6b573540368ff569e6c9bceec577762b14ded66
ChuckTheJittai <ChuckTheSheep@users.noreply.github.com>
2021-05-16 00:13:34 -0400

    Improve shadow bobbing

--- Contents/mods/Expanded Helicopter Events/media/lua/client/ExpandedHelicopter01_Main.lua
+++ Contents/mods/Expanded Helicopter Events/media/lua/client/ExpandedHelicopter01_Main.lua
@@ -698,8 +698,11 @@ function eHelicopter:update()
 	if self.shadow and (self.shadow ~= true) then
 		local shadowSize = self.shadow:getSize()
 		shadowSize = shadowSize+self.shadowFlickerRate
-		if shadowSize >= 7 or shadowSize <= 6 then
-			self.shadowFlickerRate = 0-self.shadowFlickerRate
+		print("shadowSize:"..shadowSize.."  +"..self.shadowFlickerRate)
+		if shadowSize >= 7 then
+			self.shadowFlickerRate = 0-math.abs(self.shadowFlickerRate)
+		elseif shadowSize <= 6 then
+			self.shadowFlickerRate = math.abs(self.shadowFlickerRate)
 		end
 		self.shadow:setSize(shadowSize)
 	end
