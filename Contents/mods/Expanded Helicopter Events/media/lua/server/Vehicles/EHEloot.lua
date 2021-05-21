c3ac559f7f4682d318709757e580fdafe0739eef
sharkster91 <79799951+sharkster91@users.noreply.github.com>
2021-05-21 03:05:22 -0400

    Added loot to all vehicles (needs tweaking and finalization)

--- Contents/mods/Expanded Helicopter Events/media/lua/server/Vehicles/EHEloot.lua
+++ Contents/mods/Expanded Helicopter Events/media/lua/server/Vehicles/EHEloot.lua
@@ -5,8 +5,8 @@ local distributionTable = VehicleDistributions[1]
 VehicleDistributions.UH1Seat = {
     rolls = 7,
     items ={
-        "Bag_ALICEpack_Army", 5,
-        "Vest_BulletArmy", 5,
+        "Bag_ALICEpack_Army", 9,
+        "Vest_BulletArmy", 9,
         "556Clip", 3,
         "556Clip", 3,
         "556Clip", 3,
@@ -19,11 +19,11 @@ VehicleDistributions.UH1Seat = {
         "Hat_GasMask", 3,
         "Hat_GasMask", 3,
         "Hat_GasMask", 3,
-        "AssaultRifle", 5,
-        "AssaultRifle", 5,
-        "Pistol", 3,
-        "Pistol", 3,
-        "Pistol", 3,
+        "AssaultRifle", 7,
+        "AssaultRifle", 7,
+        "Pistol", 5,
+        "Pistol", 5,
+        "Pistol", 5,
         "9mmClip", 3,
         "9mmClip", 3,
         "9mmClip", 3,
@@ -37,24 +37,210 @@ VehicleDistributions.UH1Seat = {
         "Shirt_CamoGreen", 1,
         "Jacket_ArmyCamoGreen", 1,
         "Hat_BonnieHat_CamoGreen", 1,
-        "Hat_BeretArmy", 0.5,
+        "Hat_BeretArmy", 3,
         "Shoes_ArmyBoots", 1,
         "Shirt_CamoGreen", 1,
         "Radio.WalkieTalkie5", 3,
         "HuntingKnife", 3,
         "FirstAidKit", 3,
-		"EmptyPetrolCan", 3,
-		"PetrolCan", 2,
 		"x2Scope", 0.7,
-		"x4Scope", 0.5,
-		"x8Scope", 0.3,
+        "Corpse", 10,
+        "Corpse", 10,
 	
     }
 }
 
+VehicleDistributions.UH1HSurvivalistSeat = {
+    rolls = 7,
+    items ={
+        "Bag_ALICEpack_Army", 9,
+        "Vest_BulletArmy", 9,
+        "556Clip", 3,
+        "556Clip", 3,
+        "556Clip", 3,
+        "556Box", 3,
+        "556Box", 3,
+        "556Box", 3,
+        "556Box", 3,
+        "556Box", 3,
+        "Hat_GasMask", 3,
+        "Hat_GasMask", 3,
+        "Hat_GasMask", 3,
+        "AssaultRifle", 7,
+        "AssaultRifle", 7,
+        "Pistol", 5,
+        "Pistol", 5,
+        "Pistol", 5,
+        "9mmClip", 3,
+        "9mmClip", 3,
+        "9mmClip", 3,
+        "Bullets9mm", 3,
+        "Bullets9mm", 3,
+        "Bullets9mm", 3,
+        "Bullets9mm", 3,
+        "Bullets9mm", 3,
+		"HolsterSimple", 3,
+        "Trousers_CamoGreen", 1,
+        "Shirt_CamoGreen", 1,
+        "Jacket_ArmyCamoGreen", 1,
+        "Hat_BonnieHat_CamoGreen", 1,
+        "Hat_BeretArmy", 3,
+        "Shoes_ArmyBoots", 1,
+        "Shirt_CamoGreen", 1,
+        "Radio.WalkieTalkie5", 3,
+        "HuntingKnife", 3,
+        "FirstAidKit", 3,
+		"x2Scope", 0.7,
+        "Corpse", 10,
+        "Corpse", 10,
+	
+    }
+}
 
+VehicleDistributions.UH1HMedevacSeat = {
+    rolls = 7,
+    items ={
+        "Bag_ALICEpack_Army", 9,
+        "Vest_BulletArmy", 9,
+        "556Clip", 3,
+        "556Clip", 3,
+        "556Clip", 3,
+        "556Box", 3,
+        "556Box", 3,
+        "556Box", 3,
+        "556Box", 3,
+        "556Box", 3,
+        "Hat_GasMask", 3,
+        "Hat_GasMask", 3,
+        "Hat_GasMask", 3,
+        "AssaultRifle", 7,
+        "AssaultRifle", 7,
+        "Pistol", 5,
+        "Pistol", 5,
+        "Pistol", 5,
+        "9mmClip", 3,
+        "9mmClip", 3,
+        "9mmClip", 3,
+        "Bullets9mm", 3,
+        "Bullets9mm", 3,
+        "Bullets9mm", 3,
+        "Bullets9mm", 3,
+        "Bullets9mm", 3,
+		"HolsterSimple", 3,
+        "Trousers_CamoGreen", 1,
+        "Shirt_CamoGreen", 1,
+        "Jacket_ArmyCamoGreen", 1,
+        "Hat_BonnieHat_CamoGreen", 1,
+        "Hat_BeretArmy", 3,
+        "Shoes_ArmyBoots", 1,
+        "Shirt_CamoGreen", 1,
+        "Radio.WalkieTalkie5", 3,
+        "HuntingKnife", 3,
+        "FirstAidKit", 10,
+        "FirstAidKit", 10,
+        "FirstAidKit", 10,
+        "Corpse", 10,
+        "Corpse", 10,
+        "Corpse", 10,
+		"x2Scope", 0.7,
+	
+    }
+}
 
--- add a new m113 distributions table
+VehicleDistributions.Bell206PoliceSeat = {
+    rolls = 7,
+    items ={
+        "Bag_BigHikingBag", 9,
+        "Vest_BulletPolice", 9,
+        "556Clip", 3,
+        "556Clip", 3,
+        "556Clip", 3,
+        "556Box", 3,
+        "556Box", 3,
+        "556Box", 3,
+        "556Box", 3,
+        "556Box", 3,
+        "AssaultRifle", 7,
+        "AssaultRifle", 7,
+        "Pistol", 5,
+        "Pistol", 5,
+        "Pistol", 5,
+        "9mmClip", 3,
+        "9mmClip", 3,
+        "9mmClip", 3,
+        "Bullets9mm", 3,
+        "Bullets9mm", 3,
+        "Bullets9mm", 3,
+        "Bullets9mm", 3,
+        "Bullets9mm", 3,
+		"HolsterSimple", 3,
+        "Radio.WalkieTalkie5", 3,
+        "HuntingKnife", 3,
+        "FirstAidKit", 3,
+		"x2Scope", 0.7,
+        "Corpse", 10,
+	
+    }
+}
+
+VehicleDistributions.Bell206NewsSeat = {
+    rolls = 7,
+    items ={
+        "Pistol", 5,
+        "Pistol", 5,
+        "Pistol", 5,
+        "9mmClip", 3,
+        "9mmClip", 3,
+        "9mmClip", 3,
+        "Bullets9mm", 3,
+        "Bullets9mm", 3,
+        "Bullets9mm", 3,
+        "Bullets9mm", 3,
+        "Bullets9mm", 3,
+        "Radio.WalkieTalkie5", 3,
+        "FirstAidKit", 5,
+        "Corpse", 10,
+    }
+}
+
+VehicleDistributions.FEMASupplyDrop = {
+    rolls = 20,
+    items ={
+        "FirstAidKit", 10,
+        "WaterBottleFull", 10,
+        "WaterBottleFull", 10,
+        "WaterBottleFull", 10,
+        "WaterBottleFull", 10,
+        "WaterBottleFull", 10,
+        "WaterBottleFull", 10,
+        "TinnedBeans", 10,
+        "TinnedBeans", 10,
+        "TinnedBeans", 10,
+        "TinnedBeans", 10,
+        "TinnedBeans", 10,
+        "TinnedBeans", 10,
+        "CannedCornBeef", 10,
+        "CannedCornBeef", 10,
+        "CannedCornBeef", 10,
+        "CannedCornBeef", 10,
+        "CannedCornBeef", 10,
+        "CannedCornBeef", 10,
+        "CannedCornBeef", 10,
+        "Rice", 10,
+        "Rice", 10, 
+        "Rice", 10,
+        "Rice", 10,
+        "Rice", 10, 
+        "Rice", 10,
+        "Rice", 10,
+        "Rice", 10, 
+        "Rice", 10,
+        "PreventionFlyer", 30,
+        "EmergencyFlyer", 30,
+    }
+}
+
+-- UH-1H tables
 VehicleDistributions.UH1H = {
 	
 	SeatFrontLeft = VehicleDistributions.UH1Seat;
@@ -63,8 +249,56 @@ VehicleDistributions.UH1H = {
 	SeatRearRight = VehicleDistributions.UH1Seat;
 }
 
+VehicleDistributions.UH1HSurvivalist = {
+	
+	SeatFrontLeft = VehicleDistributions.UH1HSurvivalistSeat;
+	SeatFrontRight = VehicleDistributions.UH1HSurvivalistSeat;
+	SeatRearLeft = VehicleDistributions.UH1HSurvivalistSeat;
+	SeatRearRight = VehicleDistributions.UH1HSurvivalistSeat;
+}
 
-distributionTable["UH1H"] = { Normal = VehicleDistributions.UH1H; }
 
+VehicleDistributions.UH1HMedevac = {
+	
+	SeatFrontLeft = VehicleDistributions.UH1HMedevacSeat;
+	SeatFrontRight =  VehicleDistributions.UH1HMedevacSeat;
+	SeatRearLeft =  VehicleDistributions.UH1HMedevacSeat;
+	SeatRearRight =  VehicleDistributions.UH1HMedevacSeat;
+}
+--Bell 206 tables
 
+VehicleDistributions.Bell206Police = {
+	
+	SeatFrontLeft = VehicleDistributions.Bell206PoliceSeat;
+	SeatFrontRight =  VehicleDistributions.Bell206PoliceSeat;
+	SeatRearLeft =  VehicleDistributions.Bell206PoliceSeat;
+	SeatRearRight =  VehicleDistributions.Bell206PoliceSeat;
+}
+
+VehicleDistributions.Bell206News = {
+	
+    SeatFrontLeft = VehicleDistributions.Bell206NewsSeat;
+    SeatFrontRight =  VehicleDistributions.Bell206NewsSeat;
+    SeatRearLeft =  VehicleDistributions.Bell206NewsSeat;
+    SeatRearRight =  VehicleDistributions.Bell206NewsSeat;
+}    
+
+--Supply Drop
+
+VehicleDistributions.FEMASupplyDrop = {
+	
+    TruckBed = VehicleDistributions.FEMASupplyDrop;
+
+}    
+
+--Distribution
+distributionTable["UH1HCrash"] = { Normal = VehicleDistributions.UH1H; }
+distributionTable["UH1Hsurvivalistcrash"] = { Normal = VehicleDistributions.UH1HSurvivalist; }
+distributionTable["UH1Hmedevaccrash"] = { Normal = VehicleDistributions.UH1HMedevac; }
+distributionTable["Bell206PoliceCrashed"] = { Normal = VehicleDistributions.Bell206Police; }
+distributionTable["Bell206LBMWCrashed"] = { Normal = VehicleDistributions.Bell206News; }
+
+-- FEMA Supply Drop
+
+distributionTable["FEMASupplyDrop"] = { Normal = VehicleDistributions.FEMASupplyDrop; }
 
