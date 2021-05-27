404f656d91e934201ab7647c8e0c6bd3c3e3b26a
sharkster91 <79799951+sharkster91@users.noreply.github.com>
2021-05-27 16:55:43 -0400

    Recipe/Box WIP

--- Contents/mods/Expanded Helicopter Events/media/lua/client/ExpandedHelicopter09_BoxOpening.lua
+++ Contents/mods/Expanded Helicopter Events/media/lua/client/ExpandedHelicopter09_BoxOpening.lua
@@ -1,13 +1,16 @@
 EHE_OpenBox = {}
 
 function EHE_OpenBox.FOOD(recipe, ingredients, result, player)
-	--player:getInventory():AddItems("Base.JarLid", 6)
+	player:getInventory():AddItems("EHE.NoticeFlyer", 6)
+
 end
 
 function EHE_OpenBox.MEDICAL(recipe, ingredients, result, player)
-
+	player:getInventory():AddItems("Base.FirstAidKit", 2)
+	
 end
 
 function EHE_OpenBox.SURVIVAL(recipe, ingredients, result, player)
+	player:getInventory():AddItems("Base.Torch", 2)
 
 end
