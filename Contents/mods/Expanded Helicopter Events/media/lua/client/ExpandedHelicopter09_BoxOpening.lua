5fdc0e9099a8190cf74f72221a89eca6324d2e75
ChuckTheJittai <ChuckTheSheep@users.noreply.github.com>
2021-05-27 17:13:00 -0400

    Corrects EHE_OpenBox handling

--- Contents/mods/Expanded Helicopter Events/media/lua/client/ExpandedHelicopter09_BoxOpening.lua
+++ Contents/mods/Expanded Helicopter Events/media/lua/client/ExpandedHelicopter09_BoxOpening.lua
@@ -1,16 +1,21 @@
 EHE_OpenBox = {}
 
-function EHE_OpenBox.FOOD(recipe, ingredients, result, player)
-	player:getInventory():AddItems("EHE.NoticeFlyer", 6)
-
+function EHE_OpenBox.FOOD(recipe, result, player)
+	player:getInventory():AddItems("Base.WaterBottleFull", 6)
+	player:getInventory():AddItems("Base.Rice", 6)
+	player:getInventory():AddItems("Base.TinnedBeans", 6)
 end
 
-function EHE_OpenBox.MEDICAL(recipe, ingredients, result, player)
+
+function EHE_OpenBox.MEDICAL(recipe, result, player)
 	player:getInventory():AddItems("Base.FirstAidKit", 2)
-	
+	player:getInventory():AddItems("Base.DustMask", 6)
+	player:getInventory():AddItems("Base.SurgicalGloves", 12)
 end
 
-function EHE_OpenBox.SURVIVAL(recipe, ingredients, result, player)
-	player:getInventory():AddItems("Base.Torch", 2)
 
+function EHE_OpenBox.SURVIVAL(recipe, result, player)
+	player:getInventory():AddItems("Base.Torch", 2)
+	player:getInventory():AddItems("Base.Battery", 10)
+	player:getInventory():AddItems("Base.RadioBlack", 2)
 end
