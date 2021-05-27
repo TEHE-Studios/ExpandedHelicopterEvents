1a90f744c883bb01986ebad3b4bab300cc8eedac
sharkster91 <79799951+sharkster91@users.noreply.github.com>
2021-05-27 18:22:21 -0400

    Tweaked and weighed supply boxes

--- Contents/mods/Expanded Helicopter Events/media/lua/client/ExpandedHelicopter09_BoxOpening.lua
+++ Contents/mods/Expanded Helicopter Events/media/lua/client/ExpandedHelicopter09_BoxOpening.lua
@@ -1,21 +1,22 @@
 EHE_OpenBox = {}
 
 function EHE_OpenBox.FOOD(recipe, result, player)
-	player:getInventory():AddItems("Base.WaterBottleFull", 6)
-	player:getInventory():AddItems("Base.Rice", 6)
-	player:getInventory():AddItems("Base.TinnedBeans", 6)
+	player:getInventory():AddItems("Base.WaterBottleFull", 20)
+	player:getInventory():AddItems("Base.Rice", 10)
+	player:getInventory():AddItems("Base.TinnedBeans", 10)
 end
 
 
 function EHE_OpenBox.MEDICAL(recipe, result, player)
-	player:getInventory():AddItems("Base.FirstAidKit", 2)
-	player:getInventory():AddItems("Base.DustMask", 6)
-	player:getInventory():AddItems("Base.SurgicalGloves", 12)
+	player:getInventory():AddItems("Base.FirstAidKit", 4)
+	player:getInventory():AddItems("Hat_DustMask", 6)
+	player:getInventory():AddItems("Gloves_Surgical", 12)
 end
 
 
 function EHE_OpenBox.SURVIVAL(recipe, result, player)
 	player:getInventory():AddItems("Base.Torch", 2)
 	player:getInventory():AddItems("Base.Battery", 10)
-	player:getInventory():AddItems("Base.RadioBlack", 2)
+	player:getInventory():AddItems("Radio.RadioBlack", 2)
+
 end
