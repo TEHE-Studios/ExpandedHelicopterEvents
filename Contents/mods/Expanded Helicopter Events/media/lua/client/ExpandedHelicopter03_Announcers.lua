17f64db8639171ee0f501cdd37f4d9edca6b6856
Chuckleberry Finn <6071161+ChuckTheSheep@users.noreply.github.com>
2021-06-11 08:31:43 -0400

    Adds documentation + cleans up tables

--- Contents/mods/Expanded Helicopter Events/media/lua/client/ExpandedHelicopter03_Announcers.lua
+++ Contents/mods/Expanded Helicopter Events/media/lua/client/ExpandedHelicopter03_Announcers.lua
@@ -2,44 +2,41 @@ eHelicopter_announcersLoaded = {}
 eHelicopter_announcers = {
 
 	-- ["name of announcer"] = {
-		-- ["LineCount"] = -- calculated automatically
 		-- ["Lines"] = {
 			-- ["lineID1"] = {0, "variation1", "variation2"},
-		   --- The first entry within each line is the delay added after, every variation shares the same delay
-			-- For the sake of organization It is recommended you write out the audio for "LineID"
-			-- File names have to match scripted sounds found in sounds_world.txt to be loaded
+		   --- The first entry within each line is the delay added it is spoken, every variation shares the same delay
+			-- For the sake of organization It is recommended you write out some of the spoken audio for "LineID"
+			-- File names have to match scripted sounds found in sounds_world.txt for those scripts to be loaded
 		-- }
 	-- },
 
 
 	["Raven Male"] = {
-		["LineCount"] = 0,
 		["Lines"] = {
 			["PleaseReturnToYourHomes"] = {6500, "eHeli_lineM_1a", "eHeli_lineM_1b", "eHeli_lineM_1d"},
 			["TheSituationIsUnderControl"] = {6500, "eHeli_lineM_2b", "eHeli_lineM_2c", "eHeli_lineM_2d"},
 			["ThisAreaIsNowInQuarantine"] = {7000, "eHeli_lineM_3a", "eHeli_lineM_3b", "eHeli_lineM_3c", "eHeli_lineM_3d"},
 			["DoNotTryToLeaveTheArea"] = {6500, "eHeli_lineM_5a", "eHeli_lineM_5b", "eHeli_lineM_5c", "eHeli_lineM_5d"},
-			["LockAllEntrancesAndRemainInDoors"] = {7000, "eHeli_lineM_7b", "eHeli_lineM_7c", "eHeli_lineM_7d"},
-			["AvoidContactWithOthers"] = {6500, "eHeli_lineM_8b", "eHeli_lineM_8c", "eHeli_lineM_8d"},
-			["DoNotTryToReachOutToFamilyOrRelatives"] = {7000, "eHeli_lineM_9a", "eHeli_lineM_9b", "eHeli_lineM_9c", "eHeli_lineM_9d"},
-			["AnyCriminalActivityOrLootingWillBePunishedToTheFullestExtentOfTheLaw"] = {8500, "eHeli_lineM_10a", "eHeli_lineM_10b"},
-			["AnyPersonsTryingToLeaveTheDesignatedAreaWillBeShot"] = {8500, "eHeli_lineM_6a", "eHeli_lineM_6b", "eHeli_lineM_6c", "eHeli_lineM_6d"},
+			["LockAllEntrances"] = {7000, "eHeli_lineM_7b", "eHeli_lineM_7c", "eHeli_lineM_7d"},
+			["AvoidContact"] = {6500, "eHeli_lineM_8b", "eHeli_lineM_8c", "eHeli_lineM_8d"},
+			["DoNotTryToReachOut"] = {7000, "eHeli_lineM_9a", "eHeli_lineM_9b", "eHeli_lineM_9c", "eHeli_lineM_9d"},
+			["AnyCriminalActivity"] = {8500, "eHeli_lineM_10a", "eHeli_lineM_10b"},
+			["AnyPersonsTryingToLeave"] = {8500, "eHeli_lineM_6a", "eHeli_lineM_6b", "eHeli_lineM_6c", "eHeli_lineM_6d"},
 		}
 	},
 
 
 	["Gabby Female"] = {
-		["LineCount"] = 0,
 		["Lines"] = {
 			["PleaseReturnToYourHomes"] = {5750, "eHeli_lineF_1"},
 			["TheSituationIsUnderControl"] = {6200, "eHeli_lineF_2"},
 			["ThisAreaIsNowInQuarantine"] = {6500, "eHeli_lineF_3"},
 			["DoNotTryToLeaveTheArea"] = {6500, "eHeli_lineF_5"},
-			["LockAllEntrancesAndRemainInDoors"] = {6500, "eHeli_lineF_7"},
-			["AvoidContactWithOthers"] = {5750, "eHeli_lineF_8"},
-			["DoNotTryToReachOutToFamilyOrRelatives"] = {6750, "eHeli_lineF_9"},
-			["AnyCriminalActivityOrLootingWillBePunishedToTheFullestExtentOfTheLaw"] = {8750, "eHeli_lineF_10"},
-			["AnyPersonsTryingToLeaveTheDesignatedAreaWillBeShot"] = {7500, "eHeli_lineF_6"},
+			["LockAllEntrances"] = {6500, "eHeli_lineF_7"},
+			["AvoidContact"] = {5750, "eHeli_lineF_8"},
+			["DoNotTryToReachOut"] = {6750, "eHeli_lineF_9"},
+			["AnyCriminalActivity"] = {8750, "eHeli_lineF_10"},
+			["AnyPersonsTryingToLeave"] = {7500, "eHeli_lineF_6"},
 		}
 	},
 
@@ -47,35 +44,36 @@ eHelicopter_announcers = {
 	["Jade Male A"] = {
 		["LineCount"] = 0,
 		["Lines"] = {
-			["ThisAreaIsUnderQuarantineIRepeatThisAreaIsUnderQuarantine"] = {7500, "eHeli_Jade_1a"},
-			["ForTheSafetyOfYourSelfAndYourLovedOnesYouAreAdvisedToRemainInsideAtAllTimesUnlessAbsolutelyNecessary"] = {8500, "eHeli_Jade_2a"},
-			["RepeatedAttemptsAtBreachingTheQuarantineZoneMayBeMetWithHostileResponse"] = {7250, "eHeli_Jade_3a"},
+			["ThisAreaIsUnderQuarantine"] = {7500, "eHeli_Jade_1a"},
+			["ForTheSafetyOfYourSelf"] = {8500, "eHeli_Jade_2a"},
+			["RepeatedAttemptsAtBreaching"] = {7250, "eHeli_Jade_3a"},
 		}
 	},
 
 
 	["Jade Male B"] = {
-		["LineCount"] = 0,
 		["Lines"] = {
-			["ThisAreaIsUnderQuarantineIRepeatThisAreaIsUnderQuarantine"] = {8500, "eHeli_Jade_1b"},
-			["ForTheSafetyOfYourSelfAndYourLovedOnesYouAreAdvisedToRemainInsideAtAllTimesUnlessAbsolutelyNecessary"] = {9500, "eHeli_Jade_2b"},
-			["RepeatedAttemptsAtBreachingTheQuarantineZoneMayBeMetWithHostileResponse"] = {7250, "eHeli_Jade_3b"},
+			["ThisAreaIsUnderQuarantine"] = {8500, "eHeli_Jade_1b"},
+			["ForTheSafetyOfYourSelf"] = {9500, "eHeli_Jade_2b"},
+			["RepeatedAttemptsAtBreaching"] = {7250, "eHeli_Jade_3b"},
 		}
 	},
 
 
 	["Jade Male C"] = {
-		["LineCount"] = 0,
 		["Lines"] = {
-			["ThisAreaIsUnderQuarantineIRepeatThisAreaIsUnderQuarantine"] = {8500, "eHeli_Jade_1c"},
-			["ForTheSafetyOfYourSelfAndYourLovedOnesYouAreAdvisedToRemainInsideAtAllTimesUnlessAbsolutelyNecessary"] = {9500, "eHeli_Jade_2c"},
-			["RepeatedAttemptsAtBreachingTheQuarantineZoneMayBeMetWithHostileResponse"] = {7250, "eHeli_Jade_3c"},
+			["ThisAreaIsUnderQuarantine"] = {8500, "eHeli_Jade_1c"},
+			["ForTheSafetyOfYourSelf"] = {9500, "eHeli_Jade_2c"},
+			["RepeatedAttemptsAtBreaching"] = {7250, "eHeli_Jade_3c"},
 		}
 	},
 
-
+	
 }
 
+
+
+--- Under the Hood Stuff ---
 
 --Automatically sets respective announcer's line count to use in randomized selection
 --This is needed to avoid constant length checks due to the fact Lua does not recognize #length of non-numerated lists
