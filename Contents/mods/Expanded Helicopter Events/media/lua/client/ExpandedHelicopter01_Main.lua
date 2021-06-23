72979ce96f1b003c91dddd65bdc95cd8a24be07f
ChuckTheJittai <ChuckTheSheep@users.noreply.github.com>
2021-06-22 20:56:39 -0400

    Heli damage buff

--- Contents/mods/Expanded Helicopter Events/media/lua/client/ExpandedHelicopter01_Main.lua
+++ Contents/mods/Expanded Helicopter Events/media/lua/client/ExpandedHelicopter01_Main.lua
@@ -104,7 +104,7 @@ eHelicopter.attackSpread = 3
 eHelicopter.attackHitChance = 85
 
 ---@field attackDamage number damage dealt to zombies/players on hit (gets randomized to: attackDamage * random(1 to 1.5))
-eHelicopter.attackDamage = 15
+eHelicopter.attackDamage = 20
 
 ---// UNDER THE HOOD STUFF //---
 
