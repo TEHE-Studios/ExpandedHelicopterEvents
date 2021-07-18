7713478053dc99c546b3b28e4803818e6b5e5865
ChuckTheJittai <ChuckTheSheep@users.noreply.github.com>
2021-07-18 16:18:03 -0400

    Fixes Bald Aliens

--- Contents/mods/Super Weird Helicopters/media/lua/shared/Definitions/SWH_HairOutfitDefinitions.lua
+++ Contents/mods/Super Weird Helicopters/media/lua/shared/Definitions/SWH_HairOutfitDefinitions.lua
@@ -4,8 +4,8 @@ HairOutfitDefinitions = HairOutfitDefinitions or {};
 HairOutfitDefinitions.haircutOutfitDefinition = {};
 
 local cat = {};
-cat.outfit = "1Alien";
+cat.outfit = "1AlienTourist";
 cat.haircut = "Bald:100";
 cat.beard = "None:100";
-cat.haircutColor = "0.33,0.16,0.16:100";
+cat.haircutColor = "0.99,0.99,0.99:100";
 table.insert(HairOutfitDefinitions.haircutOutfitDefinition, cat);
