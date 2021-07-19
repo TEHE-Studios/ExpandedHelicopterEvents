fe49308f8ced0e4a1a108855985758e24ea1c196
sharkster91 <79799951+sharkster91@users.noreply.github.com>
2021-07-18 22:20:05 -0400

    Update SWH_HairOutfitDefinitions.lua

--- Contents/mods/Super Weird Helicopters/media/lua/shared/Definitions/SWH_HairOutfitDefinitions.lua
+++ Contents/mods/Super Weird Helicopters/media/lua/shared/Definitions/SWH_HairOutfitDefinitions.lua
@@ -9,3 +9,9 @@ cat.haircut = "Bald:100";
 cat.beard = "None:100";
 cat.haircutColor = "0.99,0.99,0.99:100";
 table.insert(HairOutfitDefinitions.haircutOutfitDefinition, cat);
+
+local cat = {};
+cat.outfit = "1TaxMan";
+cat.haircut = "Buzzcut:50;Short:50";
+cat.beard = "None:100";
+table.insert(HairOutfitDefinitions.haircutOutfitDefinition, cat);
