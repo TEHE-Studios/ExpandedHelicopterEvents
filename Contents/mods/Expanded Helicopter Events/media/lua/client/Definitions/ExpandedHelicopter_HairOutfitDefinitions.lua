1457cab1f2bc34b2853a5984df073aa53ecf8d47
sharkster91 <79799951+sharkster91@users.noreply.github.com>
2021-10-09 05:34:02 -0400

    Created Hair Definitions for most crash zombies

---
+++ Contents/mods/Expanded Helicopter Events/media/lua/client/Definitions/ExpandedHelicopter_HairOutfitDefinitions.lua
@@ -0,0 +1,80 @@
+require 'Definitions/HairOutfitDefinitions'
+
+--Expanded Helicopter Events--
+
+local cat = {};
+cat.outfit = "EHEMilitaryPilot";
+cat.haircut = "Short:34;Bald:33;CrewCut:33;";
+cat.beard = "None:100";
+table.insert(HairOutfitDefinitions.haircutOutfitDefinition, cat);
+
+local cat = {};
+cat.outfit = "EHEPolicePilot";
+cat.haircut = "Bun:20;Kate:20;Ponytail:30";
+table.insert(HairOutfitDefinitions.haircutOutfitDefinition, cat);
+
+local cat = {};
+cat.outfit = "EHEPolicePilot";
+cat.haircut = "Short:34;Bald:33;CrewCut:33;";
+cat.beard = "None:100";
+table.insert(HairOutfitDefinitions.haircutOutfitDefinition, cat);
+
+local cat = {};
+cat.outfit = "EHEPolicePilot";
+cat.haircut = "Short:34;Bald:33;CrewCut:33;";
+table.insert(HairOutfitDefinitions.haircutOutfitDefinition, cat);
+
+local cat = {};
+cat.outfit = "EHECivilianPilot";
+cat.haircut = "Short:34;Bald:33;CrewCut:33;";
+table.insert(HairOutfitDefinitions.haircutOutfitDefinition, cat);
+
+local cat = {};
+cat.outfit = "EHECivilianPilot";
+cat.haircut = "Bun:20;Kate:20;Ponytail:30";
+table.insert(HairOutfitDefinitions.haircutOutfitDefinition, cat);
+
+local cat = {};
+cat.outfit = "EHECivilianPilot";
+cat.haircut = "Short:34;Bald:33;CrewCut:33;";
+cat.beard = "None:100";
+table.insert(HairOutfitDefinitions.haircutOutfitDefinition, cat);
+
+local cat = {};
+cat.outfit = "EHECivilianPilot";
+cat.haircut = "Bun:20;Kate:20;Ponytail:30";
+table.insert(HairOutfitDefinitions.haircutOutfitDefinition, cat);
+
+local cat = {};
+cat.outfit = "EHESoldier";
+cat.haircut = "Short:34;Bald:33;CrewCut:33;";
+cat.beard = "None:100";
+table.insert(HairOutfitDefinitions.haircutOutfitDefinition, cat);
+
+local cat = {};
+cat.outfit = "EHESoldier";
+cat.haircut = "Bun:20;Kate:20;Ponytail:30";
+table.insert(HairOutfitDefinitions.haircutOutfitDefinition, cat);
+
+local cat = {};
+cat.outfit = "EHEPoliceOfficer";
+cat.haircut = "Short:34;Bald:33;CrewCut:33;";
+cat.beard = "None:100";
+table.insert(HairOutfitDefinitions.haircutOutfitDefinition, cat);
+
+local cat = {};
+cat.outfit = "EHEPoliceOfficer";
+cat.haircut = "Bun:20;Kate:20;Ponytail:30";
+table.insert(HairOutfitDefinitions.haircutOutfitDefinition, cat);
+
+local cat = {};
+cat.outfit = "EHESamaritanPilot";
+cat.haircut = "Short:34;Bald:33;CrewCut:33;";
+table.insert(HairOutfitDefinitions.haircutOutfitDefinition, cat);
+
+local cat = {};
+cat.outfit = "EHENightwatchPilot";
+cat.haircut = "Short:34;Bald:33;CrewCut:33;";
+cat.beard = "None:100";
+table.insert(HairOutfitDefinitions.haircutOutfitDefinition, cat);
+
