f34262b199e923e0797e3df21411510ec394f09c
ChuckTheJittai <ChuckTheSheep@users.noreply.github.com>
2021-10-15 23:00:12 -0400

    Update ExpandedHelicopter02a_Presets.lua

--- Contents/mods/Expanded Helicopter Events/media/lua/client/ExpandedHelicopter02a_Presets.lua
+++ Contents/mods/Expanded Helicopter Events/media/lua/client/ExpandedHelicopter02a_Presets.lua
@@ -114,20 +114,26 @@ eHelicopter_PRESETS["patrol_only_quarantine"] = {
 	}
 
 
+eHelicopter_PRESETS["air_raid_siren"] = {
+	flightVolume = 0,
+	eventSoundEffects = {["flightSound"] = "eAirRaid"},
+	crashType = false,
+	shadow = false,
+}
+
+
 eHelicopter_PRESETS["attack_only_undead_evac"] = {
 	hostilePreference = "IsoZombie",
 	dropItems = {["EHE.EvacuationFlyer"]=250},
 	crew = {"EHEMilitaryPilot", "EHESoldier", 75, "EHESoldier", 50},
-	formationIDs = {"patrol_only", 25, {20,25}, "patrol_only", 10, {20,25}},
-	eventSoundEffects = {["additionalFlightSound"] = "eAirRaid"},
+	formationIDs = {"air_raid_siren", 100, "attack_only_undead_evac", 25, {20,25}, "attack_only_undead_evac", 10, {20,25}},
 	}
 
 
 eHelicopter_PRESETS["attack_only_undead"] = {
 	hostilePreference = "IsoZombie",
 	crew = {"EHEMilitaryPilot", "EHESoldier", 75, "EHESoldier", 50},
-	formationIDs = {"patrol_only", 25, {12,17}, "patrol_only", 10, {12,17}},
-	eventSoundEffects = {["additionalFlightSound"] = "eAirRaid"},
+	formationIDs = {"air_raid_siren", 100, "attack_only_undead", 25, {12,17}, "attack_only_undead", 10, {12,17}},
 	}
 
 
@@ -136,7 +142,7 @@ eHelicopter_PRESETS["attack_only_all"] = {
 	crashType = {"UH1HSurvivalistFuselage"},
 	crew = {"EHESurvivalistPilot", "EHESurvivalist", 75, "EHESurvivalist", 50},
 	scrapAndParts = {["vehicleSection"]="UH1HSurvivalistTail"},
-	eventSoundEffects = {["additionalFlightSound"] = "eAirRaid"},
+	formationIDs = {"air_raid_siren", 100,},
 	}
 
 
