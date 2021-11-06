07284c1e0bc2cfbe9b1882dc660e71d2cd3bebd3
ChuckTheJittai <6071161+ChuckTheSheep@users.noreply.github.com>
2021-11-06 11:37:12 -0400

    Update ExpandedHelicopter00c_FarSquarePendingSpawn.lua

--- Contents/mods/Expanded Helicopter Events/media/lua/client/ExpandedHelicopter00c_FarSquarePendingSpawn.lua
+++ Contents/mods/Expanded Helicopter Events/media/lua/client/ExpandedHelicopter00c_FarSquarePendingSpawn.lua
@@ -34,18 +34,41 @@ function farSquareSpawn.parseSquare(square)
 	for key,entry in pairs(farSquarePendingSpawns) do
 		if (not entry.spawned) and entry.x==sqX and entry.y==sqY and entry.z==sqZ then
 
-			local shiftedSquare = getOutsideSquareFromAbove(square,entry.spawnFuncType=="Vehicle")
-			local spawnFunc = farSquareSpawn["spawn"..entry.spawnFuncType]
+			local isVehicle = entry.spawnFuncType=="Vehicle"
+			local shiftedSquare = getOutsideSquareFromAbove(square,isVehicle)
 
-			if spawnFunc then
-				local spawnedObject = spawnFunc(shiftedSquare, entry.objectType)
-				if spawnedObject then
-					--[DEBUG]] print("DEBUG: farSquareSpawn.parseSquare: "..tostring(spawnedObject).." "..square:getX()..","..square:getY())
-					if entry.funcsToApply and (type(entry.funcsToApply)=="table") then
-						for k,func in pairs(entry.funcsToApply) do
-							func(spawnedObject)
-						end
+			--If isVehicle and can't spawn on square look for nearby squares.
+			if isVehicle and not shiftedSquare then
+				local aSqOutsideAbove = {}
+				for k,sq in pairs(getIsoRange(square, 2)) do
+					local outsideSq = getOutsideSquareFromAbove(sq,isVehicle)
+					if outsideSq then
+						table.insert(aSqOutsideAbove,outsideSq)
 					end
+				end
+				if #aSqOutsideAbove > 0 then
+					shiftedSquare = aSqOutsideAbove[ZombRand(#aSqOutsideAbove)+1]
+				end
+			end
+
+			if shiftedSquare then
+				local spawnFunc = farSquareSpawn["spawn"..entry.spawnFuncType]
+
+				if spawnFunc then
+					local spawnedObject = spawnFunc(shiftedSquare, entry.objectType)
+
+					if spawnedObject then
+						--[DEBUG]] print("DEBUG: farSquareSpawn.parseSquare: "..tostring(spawnedObject).." "..square:getX()..","..square:getY())
+						if entry.funcsToApply and (type(entry.funcsToApply)=="table") then
+							for k,func in pairs(entry.funcsToApply) do
+								if func then
+									func(spawnedObject)
+								end
+							end
+						end
+						farSquarePendingSpawns[key] = nil
+					end
+				else
 					farSquarePendingSpawns[key] = nil
 				end
 			else
@@ -61,6 +84,9 @@ Events.LoadGridsquare.Add(farSquareSpawn.parseSquare)
 ---@param itemType string
 ---@return InventoryItem
 function farSquareSpawn.spawnItem(square, itemType)
+	if not square or not itemType then
+		return
+	end
 	local item = square:AddWorldInventoryItem(itemType, 0, 0, 0)
 	return item
 end
@@ -69,6 +95,9 @@ end
 ---@param vehicleType string
 ---@return BaseVehicle
 function farSquareSpawn.spawnVehicle(square, vehicleType)
+	if not square or not vehicleType then
+		return
+	end
 	local vehicle = addVehicleDebug(vehicleType, IsoDirections.getRandom(), nil, square)
 	return vehicle
 end
@@ -77,6 +106,9 @@ end
 ---@param outfitID string
 ---@return ArrayList Containing : IsoZombie
 function farSquareSpawn.spawnZombie(square, outfitID)
+	if not square or not outfitID then
+		return
+	end
 	local zombies = addZombiesInOutfit(square:getX(), square:getY(), square:getZ(), 1, outfitID, 0)
 	return zombies
 end
