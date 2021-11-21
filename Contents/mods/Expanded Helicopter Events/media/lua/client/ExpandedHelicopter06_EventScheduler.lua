25f116e39cc59e3fa0b163127214630ee9acd383
ChuckTheJittai <6071161+ChuckTheSheep@users.noreply.github.com>
2021-11-21 11:32:43 -0500

    Update ExpandedHelicopter06_EventScheduler.lua

--- Contents/mods/Expanded Helicopter Events/media/lua/client/ExpandedHelicopter06_EventScheduler.lua
+++ Contents/mods/Expanded Helicopter Events/media/lua/client/ExpandedHelicopter06_EventScheduler.lua
@@ -186,7 +186,7 @@ function eHeliEvent_ScheduleNew(nightsSurvived,currentHour)
 					freq = presetFreq-1
 				end
 
-				local chance = (11-freq)*1500
+				local chance = (10-freq)*3000
 				local eventAvailable = (dayAndHourInRange or (SandboxVars.ExpandedHeli.NeverEnding==true))
 
 				--[[DEBUG] print(" processing preset: "..presetID.." a:"..tostring(dayAndHourInRange).." b:"..tostring(SandboxVars.ExpandedHeli.NeverEnding==true).." c:"..chance)--]]
