bf594897269b33b27adf9a46a4d322698ceb9546
ChuckTheJittai <6071161+ChuckTheSheep@users.noreply.github.com>
2022-01-03 22:44:07 -0500

    Shadow refactoring

--- Contents/mods/Expanded Helicopter Events/media/lua/shared/ExpandedHelicopter01f_ShadowSystem.lua
+++ Contents/mods/Expanded Helicopter Events/media/lua/shared/ExpandedHelicopter01f_ShadowSystem.lua
@@ -1,7 +1,7 @@
 require "ExpandedHelicopter00a_Util"
 
 eventShadowHandler = {}
-eventShadowHandler.storedShadows = {}
+storedShadows = {}
 
 --eventShadowHandler:setShadowPos(self.ID, self.shadowTexture, currentSquare:getX(),currentSquare:getY(),currentSquare:getX(),currentSquare:getZ())
 function eventShadowHandler:setShadowPos(ID, texture, x, y, z)
@@ -13,7 +13,7 @@ function eventShadowHandler:setShadowPos(ID, texture, x, y, z)
 		local square = getCell():getOrCreateGridSquare(x, y, 0)
 		local outsideSquare
 		if square then
-			outsideSquare = getOutsideSquareFromAbove(square)
+			outsideSquare = getOutsideSquareFromAbove(square) or square
 		end
 
 		if not texture or not x or not y or not z then
@@ -21,26 +21,25 @@ function eventShadowHandler:setShadowPos(ID, texture, x, y, z)
 			outsideSquare = nil
 		end
 
-		local shadow = eventShadowHandler.storedShadows["HELI"..ID]
+		---@type WorldMarkers.GridSquareMarker
+		local shadow = storedShadows["HELI"..ID]
 
 		if not shadow and outsideSquare then
 			print("-- no shadow but yes square")
 			shadow = getWorldMarkers():addGridSquareMarker(texture, nil, outsideSquare, 0.2, 0.2, 0.2, true, 1, 0, 0.25, 0.25)
-			eventShadowHandler.storedShadows["HELI"..ID] = shadow
+			if shadow then
+				print("-- -- yes shadow and yes square")
+				shadow:setAlpha(0.25)
+				shadow:setPosAndSize(outsideSquare:getX(),outsideSquare:getY(),outsideSquare:getZ(), 5)
+				storedShadows["HELI"..ID] = shadow
+			end
 		end
 
 		if not outsideSquare then
 			print("-- no square")
 			if shadow then
-				print("---- yes shadow : hide")
+				print("-- -- yes shadow : hide")
 				shadow:setAlpha(0)
-			end
-		else
-			print("-- yes square ")
-			if shadow then
-				print("---- yes shadow : show and place")
-				shadow:setAlpha(0.25)
-				shadow:setPos(outsideSquare:getX(),outsideSquare:getY(),outsideSquare:getZ())
 			end
 		end
 	end
