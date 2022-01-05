46c48e76574e6a940725c0a179bab9b0d7ee37eb
ChuckTheJittai <6071161+ChuckTheSheep@users.noreply.github.com>
2022-01-04 22:07:04 -0500

    Hotfix for map symbols

--- Contents/mods/Expanded Helicopter Events/media/lua/shared/EHEMapSymbols.lua
+++ Contents/mods/Expanded Helicopter Events/media/lua/shared/EHEMapSymbols.lua
@@ -1,7 +1,7 @@
 
 local function map_symbol(name)
 --- using "extra" prefix and *not* using "media/ui/LootableMaps/" should (sic!) make it compatibile with other map symbol mods, if any, for quite some time
-    MapSymbolDefinitions.getInstance():addTexture("extra:" .. name, "media/ui/LootableMaps" .. name .. ".png")
+    MapSymbolDefinitions.getInstance():addTexture("extra:" .. name, "media/ui/LootableMaps/" .. name .. ".png")
 end
 
 map_symbol("crashedHeli")
