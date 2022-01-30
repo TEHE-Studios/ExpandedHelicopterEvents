bc1624c446ecdfba7cc1cbfe1b3eb6e62309bce5
ChuckTheJittai <6071161+ChuckTheSheep@users.noreply.github.com>
2022-01-29 23:05:13 -0500

    Update ExpandedHelicopter09b_SandboxOverride.lua

--- Contents/mods/Expanded Helicopter Events/media/lua/shared/ExpandedHelicopter09b_SandboxOverride.lua
+++ Contents/mods/Expanded Helicopter Events/media/lua/shared/ExpandedHelicopter09b_SandboxOverride.lua
@@ -1,9 +1,6 @@
 --Overrides vanilla helicopter frequency on game boot
 function HelicopterSandboxOptions()
 	print("EHE: Disabling vanilla helicopter. Adding items to WorldItemRemovalList.")
-	getGameTime():setHelicopterDay(0)
-	getGameTime():setHelicopterStartHour(0)
-	getGameTime():setHelicopterEndHour(0)
 	getSandboxOptions():getOptionByName("Helicopter"):setValue(1) -- 1 = Never
 	SandboxVars.Helicopter = 1
 
