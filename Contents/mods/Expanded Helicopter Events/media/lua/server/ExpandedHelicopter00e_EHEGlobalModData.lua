4021b64bdbb9242e8b915d6cd611eb4e817e6de1
ChuckTheJittai <6071161+ChuckTheSheep@users.noreply.github.com>
2022-01-30 17:35:02 -0500

    Update ExpandedHelicopter00e_EHEGlobalModData.lua

--- Contents/mods/Expanded Helicopter Events/media/lua/server/ExpandedHelicopter00e_EHEGlobalModData.lua
+++ Contents/mods/Expanded Helicopter Events/media/lua/server/ExpandedHelicopter00e_EHEGlobalModData.lua
@@ -1,6 +1,7 @@
 require "ExpandedHelicopter00f_WeatherImpact"
 require "ExpandedHelicopter00a_Util"
 
+local ExpandedHeliEventsModData = nil
 function initExpandedHeliEventsModData()
 	local modData = ModData.getOrCreate("ExpandedHelicopterEvents")
 	if not modData.EventsOnSchedule then
@@ -14,11 +15,12 @@ function initExpandedHeliEventsModData()
 	if not modData.DaysBeforeApoc then
 		modData.DaysBeforeApoc = eHeli_getDaysBeforeApoc()
 	end
+
+	ExpandedHeliEventsModData = modData
 end
 
 function getExpandedHeliEventsModData()
-	local modData = ModData.get("ExpandedHelicopterEvents")
-	return modData
+	return ExpandedHeliEventsModData
 end
 
 Events.OnInitGlobalModData.Add(initExpandedHeliEventsModData)
