3dbd6bda82ea20e1b4704362365f415414796638
sharkster91 <79799951+sharkster91@users.noreply.github.com>
2022-02-13 12:49:08 -0500

    Create EHE_BodyLocations.lua

---
+++ Contents/mods/Expanded Helicopter Events/media/lua/shared/NPC's/EHE_BodyLocations.lua
@@ -0,0 +1,11 @@
+require 'NPCs/BodyLocations'
+--***********************************************************
+--**                    THE INDIE STONE                    **
+--***********************************************************
+
+-- Locations must be declared in render-order.
+-- Location IDs must match BodyLocation= and CanBeEquipped= values in items.txt.
+local group = BodyLocations.getGroup("Human")
+
+
+group:getOrCreateLocation("EHEPilotVest")
