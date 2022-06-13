ced8995ae4b9b3f91960693b34121a40e5f78c6a
sharkster91 <79799951+sharkster91@users.noreply.github.com>
2022-06-13 06:17:21 -0400

    Delete helicopterparts_function.lua

--- Contents/mods/Expanded Helicopter Events/media/lua/server/helicopterparts_function.lua
+++
@@ -1,9 +0,0 @@
-require "Vehicle/Vehicles"
-
--- TO DO: Learn not to just paste functions over and over
-
-function Vehicles.ContainerAccess.HelicopterInterior(vehicle, part, chr)
-    if chr:getVehicle() == vehicle then
-        return true
-    end
-end
