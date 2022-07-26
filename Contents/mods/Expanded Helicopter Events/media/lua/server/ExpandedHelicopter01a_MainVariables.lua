3bad8a9edc671a0a35dbc53f50ebd21a9747237a
Chuckleberry Finn <6071161+ChuckTheSheep@users.noreply.github.com>
2022-07-26 16:05:33 -0400

    Move field descriptions to guide file, guide still WIP

--- Contents/mods/Expanded Helicopter Events/media/lua/server/ExpandedHelicopter01a_MainVariables.lua
+++ Contents/mods/Expanded Helicopter Events/media/lua/server/ExpandedHelicopter01a_MainVariables.lua
@@ -1,102 +1,29 @@
----@class eHelicopter
 eHelicopter = {}
-
----@field forScheduling string|nil string used for scheduler; leaving it as nil means the event will not spawn from the scheduler
 eHelicopter.forScheduling = false
-
----@field schedulingFactor number multiplied against frequency to make them more or less likely - high number = more likely to be scheduled
 eHelicopter.schedulingFactor = 1
-
----@field eventSpawnWeight number This number is how many times this event is included in the scheduler's pool of events
 eHelicopter.eventSpawnWeight = 10
-
----@field eventStartDayFactor number This is number is multiplied against cutOffDay to act as when it will be able to spawn.
 eHelicopter.eventStartDayFactor = 0
-
----@field doNotListForTwitchIntegration
 eHelicopter.doNotListForTwitchIntegration = false
-
----@field ignoreNeverEnding
 eHelicopter.ignoreNeverEnding = false
-
----@field eventSpecialDates table table of specific in-game months/day tables; inGameDates/systemDates (table of tables)
---- EXAMPLES: {{1,1}} = 1st month and 1st day only
----           {{1}} = Entire 1st Month
----           {{2}, {3,15}} = Entire 2nd month to 3rd Month 15th day.
----If no day is provided it is assumed to use the entire month
-eHelicopter.eventSpecialDates = false --example: { systemDates = {{1,1}}, inGameDates = {{2}, {3,15}}}
-
----@field eventCutOffDayFactor number This is multiplied against cutOffDay to act as the day this event no longer spawns
+eHelicopter.eventSpecialDates = false
 eHelicopter.eventCutOffDayFactor = 0.34
-
----@field radioChatter string
 eHelicopter.radioChatter = "AEBS_Choppah"
-
----@field flightHours table
 eHelicopter.flightHours = {5, 22}
-
----@field hoverOnTargetDuration number|boolean How long the helicopter will hover over the player, this is subtracted from every tick
 eHelicopter.hoverOnTargetDuration = false
-
----@field searchForTargetDurationMS number How long the helicopter will search for last seen targets
 eHelicopter.searchForTargetDuration = 30000
-
----@field shadow boolean | WorldMarkers.GridSquareMarker
 eHelicopter.shadow = true
-
----@field shadowTexture string
 eHelicopter.shadowTexture = "helicopter_shadow"
-
----@field eventMarkerIcon string
 eHelicopter.eventMarkerIcon = "media/ui/helievent.png"
-
----@field crashType boolean
 eHelicopter.crashType = {"UH1HFuselage"}
-
----@field addedCrashChance number
 eHelicopter.addedCrashChance = 0
-
----Useful for submodders seeking to add more functionality to events.
----Simply make your preset's table filled with the names of functions you want to call.
----NOTE: Presets' file must be loaded after any called function's file to work.
----If you want your event to occur only once simply set the entry to false afterwards.
----
----All functions called have the following arguments: self (eHelicopter)
----OnCrash has the additional argument of: currentSquare (IsoGridSquare)
----OnAttack has the additional argument of: targetHostile (IsoObject|IsoMovingObject|IsoGameCharacter|IsoPlayer|IsoZombie)
----@field addedFunctionsToEvents table
-eHelicopter.addedFunctionsToEvents = {
-	["OnCrash"] = false,
-	["OnHover"] = false,
-	["OnFlyaway"] = false,
-	["OnAttack"] = false,
-	["OnSpawnCrew"] = false,
-	["OnArrived"] = false}
-
----@field scrapVehicles table
+eHelicopter.addedFunctionsToEvents = { ["OnCrash"] = false, ["OnHover"] = false, ["OnFlyaway"] = false, ["OnAttack"] = false, ["OnSpawnCrew"] = false, ["OnArrived"] = false}
 eHelicopter.scrapVehicles = {"UH1HTail"} --{"Base.TYPE","Base.TYPE"}
-
----@field scrapItems table
 eHelicopter.scrapItems = {"EHE.UH1HHalfSkirt", "EHE.UH1HRotorBlade", 2, "EHE.Bell206TailBlade", 2, "Base.ScrapMetal", 10}
-
----@field crew table list of IDs and chances (similar to how loot distribution is handled)
----Example: crew = {"pilot", 100, "crew", 75, "crew", 50}
----If there is no number following a string a chance of 100% will be applied.
 eHelicopter.crew = {"AirCrew", 100}
-
----@field formation table table of IDs to generate follower helis
 eHelicopter.formationIDs = {}
-
----@field dropItems table
 eHelicopter.dropItems = false
-
----@field dropPackages table
 eHelicopter.dropPackages = false
-
----@field looperEventIDs table
 eHelicopter.looperEventIDs = {["additionalFlightSound"]=true, ["flightSound"]=true}
-
----@field eventSoundEffects table
 eHelicopter.eventSoundEffects = {
 	["hoverOverTarget"]="IGNORE",
 	["flyOverTarget"]="IGNORE",
@@ -114,67 +41,18 @@ eHelicopter.eventSoundEffects = {
 	["crashEvent"] = "eHelicopterCrash",
 }
 
----@field announcerVoice string
 eHelicopter.announcerVoice = false
-
----@field randomEdgeStart boolean
 eHelicopter.randomEdgeStart = true
-
----example: {["preset1"]=0,["preset2"]=25,["preset3"]=50} = at 0% (days out of cutoff day) preset1 is chosen, at 25% preset2 is chosen, etc.
----@field presetProgression table Table of presetIDs and corresponding % preset is compared to Days/CuttOffDay
 eHelicopter.presetProgression = false
-
----Example: {"preset1",2,"preset2","preset3",4} = a list equal to {"preset1","preset1","preset2","preset3","preset3","preset3","preset3"}
----@field presetRandomSelection table Table of presetIDs and optional corresponding weight (weight is 1 if none found) in list to be chosen from.
 eHelicopter.presetRandomSelection = false
-
----@field speed number
 eHelicopter.speed = 1
-
----@field topSpeedFactor number speed x this = top "speed"
 eHelicopter.topSpeedFactor = 1.5
-
----@field flightVolume number
 eHelicopter.flightVolume = 75
-
----@field hostilePreference string
----set to 'false' for *none*, otherwise has to be 'IsoPlayer' or 'IsoZombie' or 'IsoGameCharacter'
 eHelicopter.hostilePreference = false
-
----@field attackDelay number delay in milliseconds between attacks
 eHelicopter.attackDelay = 60
-
----@field attackScope number number of rows from "center" IsoGridSquare out
---- **area formula:** ((Scope*2)+1) ^2
----
---- scope:⠀0=1x1;⠀1=3x3;⠀2=5x5;⠀3=7x7;⠀4=9x9
 eHelicopter.attackScope = 1
-
----@field attackSpread number number of rows made of "scopes" from center-scope out
----**formula for ScopeSpread area:**
----
----((Scope * 2)+1) * ((Spread * 2)+1) ^2
----
---- **Examples:**
----
----⠀  ⠀*scope* 🡇
---- -----------------------------------
---- *spread*⠀🡆 ⠀ | 00 | 01 | 02 | 03 |
---- -----------------------------------
---- ⠀  ⠀⠀⠀ ⠀| 00 | 01 | 09 | 25 | 49 |
---- -----------------------------------
---- ⠀  ⠀⠀⠀ ⠀| 01 | 09 | 81 | 225 | 441 |
---- -----------------------------------
---- ⠀  ⠀⠀⠀⠀  | 02 | 25 | 225 | 625 | 1225 |
---- -----------------------------------
---- ⠀  ⠀⠀⠀  ⠀| 03 | 49 | 441 | 1225 | 2401 |
---- -----------------------------------
 eHelicopter.attackSpread = 3
-
----@field attackHitChance number multiplied against chance to hit in attacking
 eHelicopter.attackHitChance = 85
-
----@field attackDamage number damage dealt to zombies/players on hit (gets randomized to: attackDamage * random(1 to 1.5))
 eHelicopter.attackDamage = 10
 
 ---// UNDER THE HOOD STUFF //---
---
+++ Contents/mods/Expanded Helicopter Events/media/lua/shared/ExpandedHelicopter_PRESETGUIDE.lua
@@ -0,0 +1,481 @@
+if 1==1 then return end ---Remove this Line to get started.
+
+-----------------------------------------------------------
+--- THIS FILE MUST GO IN MEDIA/SHARED/ IN ORDER TO WORK ---
+-----------------------------------------------------------
+require "ExpandedHelicopter02a_Presets"
+---Only include variables you want changed in the preset.
+
+---@class eHelicopter
+---@field forScheduling boolean false | string used for scheduler; leaving it as false means the event will not spawn from the scheduler
+---@field schedulingFactor number multiplied against frequency to make them more or less likely - high number = more likely to be scheduled
+---@field eventSpawnWeight number This number is how many times this event is included in the scheduler's pool of events
+---@field eventStartDayFactor number This is number is multiplied against cutOffDay to act as when it will be able to spawn.
+---@field doNotListForTwitchIntegration boolean default is false
+---@field ignoreNeverEnding boolean default is false
+
+---@field eventSpecialDates table table of specific in-game months/day tables; inGameDates/systemDates (table of tables)
+--- EXAMPLES: {{1,1}} = 1st month and 1st day only
+---           {{1}} = Entire 1st Month
+---           {{2}, {3,15}} = Entire 2nd month to 3rd Month 15th day.
+---If no day is provided it is assumed to use the entire month
+--- example: { systemDates = {{1,1}}, inGameDates = {{2}, {3,15}}}
+
+---@field eventCutOffDayFactor number This is multiplied against cutOffDay to act as the day this event no longer spawns
+---@field radioChatter string default: "AEBS_Choppah" - you will need to add your own translation file for unique strings.
+---@field flightHours table table of numbers, example, flightHours = {5, 22}
+---@field hoverOnTargetDuration number|boolean How long the helicopter will hover over the player, this is subtracted from every tick, default is false
+---@field searchForTargetDurationMS number How long the helicopter will search for last seen targets in ticks
+---@field shadow boolean | WorldMarkers.GridSquareMarker if set to true a shadow (WorldMarkers.GridSquareMarker) will be created
+---@field shadowTexture string texture filename to use for the shadow, default: "helicopter_shadow"
+---@field eventMarkerIcon string texture filename to use for markers, default: "media/ui/helievent.png"
+---@field crashType boolean|table is false no wreck will be spawned, otherwise use a table - only 1 entry will be selected at random, default: {"UH1HFuselage"}
+---@field addedCrashChance number additional % to add to crash chance , default is 0.
+
+---@field addedFunctionsToEvents table table of IDs for additional function calls on specific events, these functions need to be defined/before above the presets file
+---Useful for submodders seeking to add more functionality to events.
+---Simply make your preset's table filled with the names of functions you want to call.
+---NOTE: Presets' file must be loaded after any called function's file to work.
+---If you want your event to occur only once simply set the entry to false afterwards.
+---All functions called have the following arguments: self (eHelicopter)
+---OnCrash has the additional argument of: currentSquare (IsoGridSquare)
+---OnAttack has the additional argument of: targetHostile (IsoObject|IsoMovingObject|IsoGameCharacter|IsoPlayer|IsoZombie)
+---{ ["OnCrash"] = false, ["OnHover"] = false, ["OnFlyaway"] = false, ["OnAttack"] = false, ["OnSpawnCrew"] = false, ["OnArrived"] = false}
+
+---@field scrapVehicles table table of additional vehicles to act as extra large pieces, default: {"UH1HTail"} --{"Base.TYPE","Base.TYPE"}
+---@field scrapItems table table of additional vehicles to act as extra small pieces, default: {"EHE.UH1HHalfSkirt", "EHE.UH1HRotorBlade", 2, "EHE.Bell206TailBlade", 2, "Base.ScrapMetal", 10}
+
+---@field crew table list of IDs and chances (similar to how loot distribution is handled)
+---Example: crew = {"pilot", 100, "crew", 75, "crew", 50}
+---If there is no number following a string a chance of 100% will be applied.
+---default: {"AirCrew", 100}
+
+---@field formation table table of IDs to generate follower helis, default: {}
+---@field dropItems table|boolean table of items IDs or false to disable
+---@field dropPackages table|boolean table of vehicle IDs or false to disable
+
+---@field looperEventIDs table of soundeffectIDs to act as loops
+---(this is a workaround to handle looped sounds in MP)
+---default: {["additionalFlightSound"]=true, ["flightSound"]=true}
+
+---@field eventSoundEffects table table of additional soundeffectIDs with corresponding "IGNORE" or sound filenames
+---default:
+---eHelicopter.eventSoundEffects = {
+---    ["hoverOverTarget"]="IGNORE",
+---    ["flyOverTarget"]="IGNORE",
+---    ["lostTarget"]="IGNORE",
+---    ["foundTarget"]="IGNORE",
+---    ["droppingPackage"]="IGNORE",
+---    ["additionalAttackingSound"]="IGNORE",
+---    ["additionalFlightSound"]="IGNORE", --LOOPED
+---    ["soundAtEventOrigin"]="IGNORE",
+---
+---    ["attackSingle"] = "eHeli_machine_gun_fire_single",
+---    ["attackLooped"] = "eHeli_machine_gun_fire_looped",
+---    ["attackImpacts"] = "eHeli_fire_impact",
+---    ["flightSound"] = "eMiliHeli", --LOOPED
+---    ["crashEvent"] = "eHelicopterCrash",
+---}
+
+---@field announcerVoice string string ID of announcer, or false to disable, default: false
+
+---@field randomEdgeStart boolean if the edge the event starts from will be random
+--- default is true, false will force the edge to always be the closest
+
+---@field presetProgression table Table of presetIDs and corresponding % preset is compared to Days/CuttOffDay
+---example: {["preset1"]=0,["preset2"]=25,["preset3"]=50} = at 0% (days out of cutoff day) preset1 is chosen, at 25% preset2 is chosen, etc.
+---default: false
+
+---@field presetRandomSelection table Table of presetIDs and optional corresponding weight (weight is 1 if none found) in list to be chosen from.
+---Example: {"preset1",2,"preset2","preset3",4} = a list equal to {"preset1","preset1","preset2","preset3","preset3","preset3","preset3"}
+---default: false
+
+---@field speed number
+eHelicopter.speed = 1
+
+---@field topSpeedFactor number speed x this = top "speed"
+eHelicopter.topSpeedFactor = 1.5
+
+---@field flightVolume number
+eHelicopter.flightVolume = 75
+
+---@field hostilePreference string
+---set to 'false' for *none*, otherwise has to be 'IsoPlayer' or 'IsoZombie' or 'IsoGameCharacter'
+eHelicopter.hostilePreference = false
+
+---@field attackDelay number delay in milliseconds between attacks
+eHelicopter.attackDelay = 60
+
+---@field attackScope number number of rows from "center" IsoGridSquare out
+--- **area formula:** ((Scope*2)+1) ^2
+---
+--- scope:⠀0=1x1;⠀1=3x3;⠀2=5x5;⠀3=7x7;⠀4=9x9
+eHelicopter.attackScope = 1
+
+---@field attackSpread number number of rows made of "scopes" from center-scope out
+---**formula for ScopeSpread area:**
+---
+---((Scope * 2)+1) * ((Spread * 2)+1) ^2
+---
+--- **Examples:**
+---
+---⠀  ⠀*scope* 🡇
+--- -----------------------------------
+--- *spread*⠀🡆 ⠀ | 00 | 01 | 02 | 03 |
+--- -----------------------------------
+--- ⠀  ⠀⠀⠀ ⠀| 00 | 01 | 09 | 25 | 49 |
+--- -----------------------------------
+--- ⠀  ⠀⠀⠀ ⠀| 01 | 09 | 81 | 225 | 441 |
+--- -----------------------------------
+--- ⠀  ⠀⠀⠀⠀  | 02 | 25 | 225 | 625 | 1225 |
+--- -----------------------------------
+--- ⠀  ⠀⠀⠀  ⠀| 03 | 49 | 441 | 1225 | 2401 |
+--- -----------------------------------
+eHelicopter.attackSpread = 3
+
+---@field attackHitChance number multiplied against chance to hit in attacking
+eHelicopter.attackHitChance = 85
+
+---@field attackDamage number damage dealt to zombies/players on hit (gets randomized to: attackDamage * random(1 to 1.5))
+eHelicopter.attackDamage = 10
+
+eHelicopter_PRESETS = eHelicopter_PRESETS or {}
+
+--[[
+eHelicopter_PRESETS["id_name"] = {
+		variable = {values}
+	}
+]]
+
+
+--- forScheduling string used for scheduler; leaving it as nil means the event will not spawn from the scheduler
+--- schedulingFactor number multiplied against frequency to make them more or less likely - high number = more likely to be scheduled
+--- eventSpawnWeight number This number is how many times this event is included in the scheduler's pool of events
+--- eventStartDayFactor number This is number is multiplied against cutOffDay to act as when it will be able to spawn.
+--- eventCutOffDayFactor number This is multiplied against cutOffDay to act as the day this event no longer spawns
+
+--- doNotListForTwitchIntegration
+--- ignoreNeverEnding
+
+eHelicopter_PRESETS["military"] = {
+    announcerVoice = true,
+    forScheduling = true,
+    crew = {"EHEMilitaryPilot", "EHESoldier", 75, "EHESoldier", 50},
+    crashType = {"UH60GreenFuselage"},
+    scrapItems = {"EHE.UH60Elevator", 1, "EHE.UH60WindowGreen", 1, "EHE.UH60DoorGreen", 1, "Base.ScrapMetal", 5},
+    scrapVehicles = {"UH60GreenTail"},
+    eventSpawnWeight = 20,
+    schedulingFactor = 1.5,
+    presetProgression = {
+        ["patrol_only"] = 0,
+        ["patrol_only_emergency"] = 0.0066,
+        ["military_recon_hover"] = 0.0070,
+        ["patrol_only_quarantine"] = 0.0165,
+        ["attack_only_undead_evac"] = 0.033,
+        ["attack_only_undead"] = 0.066,
+        ["cargo_helicopter"] = 0.1900,
+        ["attack_only_all"] = 0.2145,
+    }
+}
+
+eHelicopter_PRESETS["patrol_only"] = {
+    inherit = {"military"},
+}
+
+-- EmergencyFlyer QuarantineFlyer EvacuationFlyer NoticeFlyer PreventionFlyer
+eHelicopter_PRESETS["patrol_only_emergency"] = {
+    inherit = {"military"},
+    dropItems = {["EHE.EmergencyFlyer"]=250},
+    announcerVoice = "FlyerChoppers",
+    formationIDs = {"patrol_only_emergency", 25, {20,25}, "patrol_only_emergency", 10, {20,25}},
+}
+
+eHelicopter_PRESETS["patrol_only_quarantine"] = {
+    inherit = {"military"},
+    dropItems = {["EHE.QuarantineFlyer"]=250},
+    announcerVoice = "FlyerChoppers",
+    formationIDs = {"patrol_only_quarantine", 25, {20,25}, "patrol_only_quarantine", 10, {20,25}},
+}
+
+eHelicopter_PRESETS["attack_only_undead_evac"] = {
+    announcerVoice = false,
+    inherit = {"military"},
+    hostilePreference = "IsoZombie",
+    dropItems = {["EHE.EvacuationFlyer"]=250},
+    formationIDs = {"attack_only_undead_evac", 25, {20,25}, "attack_only_undead_evac", 10, {20,25}},--"air_raid",
+}
+
+eHelicopter_PRESETS["attack_only_undead"] = {
+    inherit = {"military"},
+    announcerVoice = false,
+    hostilePreference = "IsoZombie",
+    formationIDs = {"attack_only_undead", 25, {12,17}, "attack_only_undead", 10, {12,17}},--"air_raid",
+}
+
+eHelicopter_PRESETS["attack_only_all"] = {
+    inherit = {"military"},
+    announcerVoice = false,
+    hostilePreference = "IsoGameCharacter",
+    crashType = {"UH60GreenFuselage"},
+    scrapItems = {"EHE.UH60Elevator", 1, "EHE.UH60WindowGreen", 1, "EHE.UH60DoorGreen", 1, "Base.ScrapMetal", 10},
+    scrapVehicles = {"UH60GreenTail"},
+    radioChatter = "AEBS_HostileMilitary",
+    --formationIDs = {"air_raid"},
+}
+
+eHelicopter_PRESETS["cargo_helicopter"] = {
+    inherit = {"military"},
+    announcerVoice = false,
+    crashType = false,
+    crashType = {"UH60GreenFuselage"},
+    scrapItems = {"EHE.UH60Elevator", 1, "EHE.UH60WindowGreen", 1, "EHE.UH60DoorGreen", 1, "Base.ScrapMetal", 10},
+    eventSoundEffects = {
+        ["flightSound"] = "eMiliHeliCargo",
+    },
+}
+
+eHelicopter_PRESETS["military_recon_hover"] = {
+    inherit = {"military"},
+    announcerVoice = false,
+    speed = 1.5,
+    crashType = false,
+    hoverOnTargetDuration = {200,400},
+}
+
+eHelicopter_PRESETS["FEMA_drop"] = {
+    inherit = {"military"},
+    announcerVoice = false,
+    forScheduling = true,
+    crashType = {"UH60MedevacFuselage"},
+    hoverOnTargetDuration = 500,
+    dropPackages = {"FEMASupplyDrop"},
+    dropItems = {["EHE.QuarantineFlyer"]=150},
+    speed = 0.9,
+    scrapItems = {"EHE.UH60Elevator", 1, "EHE.UH60WindowGreen", 1, "EHE.UH60DoorMedevac", 1, "Base.ScrapMetal", 5},
+    scrapVehicles = {"UH60GreenTail"},
+    eventSoundEffects = {
+        ["foundTarget"] = "eHeli_AidDrop_2",
+        ["droppingPackage"] = "eHeli_AidDrop_1and3",
+    },
+    formationIDs = {"patrol_only", 25, {12,17}, "patrol_only", 10, {12,17}},
+    radioChatter = "AEBS_SupplyDrop",
+    eventStartDayFactor = 0.034,
+    eventCutOffDayFactor = 0.2145,
+}
+
+
+eHelicopter_PRESETS["jet"] = {
+    speed = 15,
+    topSpeedFactor = 2,
+    flightVolume = 25,
+    eventSoundEffects = {["flightSound"] = "eJetFlight"},
+    crashType = false,
+    shadow = false,
+    eventMarkerIcon = "media/ui/jet.png",
+    forScheduling = true,
+    schedulingFactor = 4,
+    eventSpawnWeight = 5,
+    radioChatter = "AEBS_JetPass",
+}
+
+eHelicopter_PRESETS["air_raid"] = {
+    doNotListForTwitchIntegration = true,
+    crashType = false,
+    shadow = false,
+    speed = 0.5,
+    topSpeedFactor = 3,
+    flightVolume = 0,
+    eventSoundEffects = {["flightSound"]="IGNORE",["soundAtEventOrigin"] = "eAirRaid"},
+    eventMarkerIcon = false,
+    forScheduling = true,
+    flightHours = {11, 11},
+    eventSpawnWeight = 50,
+    schedulingFactor = 99999,
+    eventStartDayFactor = 0.067,
+    eventCutOffDayFactor = 0.067,
+    ignoreNeverEnding = true,
+    radioChatter = "AEBS_AirRaid",
+}
+
+eHelicopter_PRESETS["jet_bombing"] = {
+    doNotListForTwitchIntegration = true,
+    speed = 18,
+    topSpeedFactor = 2,
+    flightVolume = 25,
+    eventSoundEffects = {["flightSound"] = "eJetFlight", ["soundAtEventOrigin"] = "eCarpetBomb"},
+    crashType = false,
+    shadow = false,
+    eventMarkerIcon = "media/ui/jet.png",
+    forScheduling = true,
+    flightHours = {12, 12},
+    eventSpawnWeight = 50,
+    schedulingFactor = 99999,
+    eventStartDayFactor = 0.067,
+    eventCutOffDayFactor = 0.067,
+    ignoreNeverEnding = true,
+    radioChatter = "AEBS_JetBombing",
+}
+
+eHelicopter_PRESETS["news_chopper"] = {
+    presetRandomSelection = {"news_chopper_hover", 1, "news_chopper_fleeing", 2, },
+    eventSoundEffects = { ["additionalFlightSound"] = "eHeli_newscaster", ["flightSound"] = "eHelicopter", },
+    speed = 1,
+    crew = {"EHECivilianPilot", "EHENewsReporterVest", "EHENewsReporterVest", 40},
+    crashType = {"Bell206LBMWFuselage"},
+    scrapItems = {"EHE.Bell206HalfSkirt", "EHE.Bell206RotorBlade1", 2, "EHE.Bell206RotorBlade2", 2,  "EHE.Bell206TailBlade", 2, "Base.ScrapMetal", 10},
+    scrapVehicles = {"Bell206LBMWTail"},
+    forScheduling = true,
+    eventStartDayFactor = 0.067,
+    eventCutOffDayFactor = 0.22,
+    radioChatter = "AEBS_UnauthorizedEntryNews",
+}
+
+eHelicopter_PRESETS["news_chopper_hover"] = {
+    inherit = {"news_chopper"},
+    hoverOnTargetDuration = {750,1200},
+}
+
+eHelicopter_PRESETS["news_chopper_fleeing"] = {
+    inherit = {"news_chopper"},
+    speed = 1.6,
+}
+
+eHelicopter_PRESETS["police"] = {
+    presetRandomSelection = {"police_heli_emergency",3, "police_heli_firing",2},
+    crashType = {"Bell206PoliceFuselage"},
+    crew = {"EHEPolicePilot", "EHEPoliceOfficer", "EHEPoliceOfficer", 75},
+    scrapItems = {"EHE.Bell206HalfSkirt", "EHE.Bell206RotorBlade1", 2, "EHE.Bell206RotorBlade2", 2,  "EHE.Bell206TailBlade", 2, "Base.ScrapMetal", 10},
+    scrapVehicles = {"Bell206PoliceTail"},
+    announcerVoice = "Police",
+    eventSoundEffects = {
+        ["foundTarget"] = "eHeli_PoliceSpotted",
+    },
+    forScheduling = true,
+    eventStartDayFactor = 0.067,
+    eventCutOffDayFactor = 0.22,
+    radioChatter = "AEBS_UnauthorizedEntryPolice",
+}
+
+eHelicopter_PRESETS["police_heli_emergency"] = {
+    inherit = {"police"},
+    speed = 1.5,
+    eventSoundEffects = {
+        ["additionalFlightSound"] = "eHeliPoliceSiren",
+        ["flightSound"] = "eHelicopter",
+    },
+
+}
+
+eHelicopter_PRESETS["police_heli_firing"] = {
+    inherit = {"police"},
+    attackDelay = 1700,
+    attackSpread = 4,
+    speed = 1.0,
+    attackHitChance = 95,
+    attackDamage = 12,
+    hostilePreference = "IsoZombie",
+    eventSoundEffects = {
+        ["attackSingle"] = "eHeliAlternatingShots",
+        ["attackLooped"] = "eHeliAlternatingShots",
+        ["additionalFlightSound"] = "eHeliPoliceSiren",
+        ["flightSound"] = "eHelicopter",
+    },
+    hoverOnTargetDuration = {375,575},
+}
+
+
+eHelicopter_PRESETS["samaritan_drop"] = {
+    crashType = false,
+    crew = {"EHESurvivorPilot", 100, 0},
+    dropPackages = {"SurvivorSupplyDrop"},
+    speed = 1.0,
+    eventMarkerIcon = "media/ui/jet.png",
+    eventSoundEffects = {["flightSound"] = "ePropPlane"},
+    forScheduling = true,
+    eventCutOffDayFactor = 1,
+    eventStartDayFactor = 0.48,
+    eventSpawnWeight = 3,
+    radioChatter = "AEBS_SamaritanDrop"
+}
+
+
+eHelicopter_PRESETS["survivor_heli"] = {
+    speed = 1.5,
+    crashType = {"Bell206SurvivalistFuselage"},
+    crew = {"EHESurvivorPilot", 100, 0, "EHESurvivor", 100, 0, "EHESurvivor", 75, 0},
+    eventSoundEffects = {
+        ["flightSound"] = "eHelicopter",
+    },
+    scrapItems = {"EHE.Bell206HalfSkirt", "EHE.Bell206RotorBlade1", 2, "EHE.Bell206RotorBlade2", 2,  "EHE.Bell206TailBlade", 2, "Base.ScrapMetal", 10},
+    scrapVehicles = {"Bell206SurvivalistTail"},
+    forScheduling = true,
+    crashType = false,
+    eventCutOffDayFactor = 1,
+    eventStartDayFactor = 0.48,
+    radioChatter = "AEBS_SurvivorHeli",
+}
+
+
+eHelicopter_PRESETS["raiders"] = {
+    presetRandomSelection = {"raider_heli_passive",3,"raider_heli_harasser",1,"raider_heli_hostile",1},
+    crashType = {"UH60GreenFuselage"},
+    scrapItems = {"EHE.UH60Elevator", 1, "EHE.UH60WindowGreen", 1, "EHE.UH60DoorGreen", 1, "Base.ScrapMetal", 10},
+    scrapVehicles = {"UH60GreenTail"},
+    addedFunctionsToEvents = {["OnFlyaway"] = eHelicopter_dropTrash},
+    crew = {"EHERaiderPilot", 100, 0, "EHERaider", 100, 0, "EHERaider", 100, 0, "EHERaider", 100, 0, "EHERaiderLeader", 75, 0},
+    forScheduling = true,
+    eventCutOffDayFactor = 1,
+    eventStartDayFactor = 0.48,
+    radioChatter = "AEBS_Raiders",
+}
+
+
+eHelicopter_PRESETS["raider_heli_passive"] = {
+    inherit = {"raiders"},
+    speed = 0.5,
+    flightVolume = 750,
+    crashType = false,
+    eventSoundEffects = {
+        ["flightSound"] = "eMiliHeli",
+        ["additionalFlightSound"] = "eHeliMusicPassive",
+    },
+}
+
+eHelicopter_PRESETS["raider_heli_harasser"] = {
+    inherit = {"raiders"},
+    hoverOnTargetDuration = {450,850},
+    speed = 2,
+    attackDelay = 1000,
+    attackSpread = 4,
+    attackHitChance = 70,
+    attackDamage = 50,
+    flightVolume = 750,
+    crashType = false,
+    hostilePreference = "IsoZombie",
+    eventSoundEffects = {
+        ["flightSound"] = "eMiliHeli",
+        ["attackSingle"] = "eHeliAlternatingShots",
+        ["attackLooped"] = "eHeliAlternatingShots",
+        ["additionalFlightSound"] = "eHeliMusicAggressive",
+    },
+}
+
+
+eHelicopter_PRESETS["raider_heli_hostile"] = {
+    inherit = {"raiders"},
+    hoverOnTargetDuration = {650,1500},
+    speed = 1.5,
+    attackDelay = 650,
+    attackSpread = 4,
+    attackHitChance = 60,
+    attackDamage = 10,
+    flightVolume = 750,
+    crashType = false,
+    hostilePreference = "IsoPlayer",
+    eventSoundEffects = {
+        ["flightSound"] = "eMiliHeli",
+        ["attackSingle"] = "eHeliAlternatingShots",
+        ["attackLooped"] = "eHeliAlternatingShots",
+        ["additionalFlightSound"] = "eHeliMusicAggressive",
+    },
+}
