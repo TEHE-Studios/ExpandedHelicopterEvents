3c708867c7849a0da0e5662012b9f9d85458fbfd
Chuckleberry Finn <6071161+ChuckTheSheep@users.noreply.github.com>
2022-07-27 14:26:13 -0400

    ECC Translation Update

--- Contents/mods/Expanded Helicopter Events/media/lua/shared/ExpandedHelicopter09_EasyConfigOptions.lua
+++ Contents/mods/Expanded Helicopter Events/media/lua/shared/ExpandedHelicopter09_EasyConfigOptions.lua
@@ -5,7 +5,6 @@ require "EasyConfigChucked1_Main"
 eHelicopterSandbox = eHelicopterSandbox or {}
 
 eHelicopterSandbox.modId = "ExpandedHelicopterEvents" -- needs to the same as in your mod.info
-eHelicopterSandbox.name = "Expanded Helicopter Events" -- the name that will be shown in the MOD tab
 eHelicopterSandbox.menuSpecificAccess = "mainmenu"
 
 eHelicopterSandbox.config = eHelicopterSandbox.config or {}
@@ -13,22 +12,22 @@ eHelicopterSandbox.config.eventMarkersOn = true
 eHelicopterSandbox.config.resetEvents = false
 
 eHelicopterSandbox.menu = eHelicopterSandbox.menu or {}
-eHelicopterSandbox.menu.sandBoxMovedText = {type = "Text", text = "Configuration options can be found in sandbox options.", r=1, g=0.2, b=0.2, a=0.65,customX=-56}
+eHelicopterSandbox.menu.sandBoxMovedText = {type = "Text", r=1, g=0.2, b=0.2, a=0.65,customX=-56}
 ---voices added automatically
 
 
 function loadAnnouncersToConfig()
 
-	if eHelicopterSandbox.menu["voiceSpaceB"] then
-		eHelicopterSandbox.menu["voiceSpaceB"] = nil
+	if eHelicopterSandbox.menu.voiceSpaceB then
+		eHelicopterSandbox.menu.voiceSpaceB = nil
 	end
 
-	eHelicopterSandbox.menu["voiceSpaceA"] = {type = "Space"}
-	eHelicopterSandbox.menu["voiceTitle"] = {type = "Text", text = "Voice Packs", }
+	eHelicopterSandbox.menu.voiceSpaceA = {type = "Space"}
+	eHelicopterSandbox.menu.voiceTitle = {type = "Text", }
 
 	for k,params in pairs(eHelicopter_announcers) do
 		if params.DoNotDisplayOnOptions ~= true then
-			eHelicopterSandbox.menu[k] = {type = "Tickbox", title = k, tooltip = "", }
+			eHelicopterSandbox.menu[k] = {type = "Tickbox", title = k, tooltip = "", noTranslate=true}
 			eHelicopterSandbox.config[k] = eHelicopterSandbox.config[k] or true
 		end
 	end
@@ -38,21 +37,22 @@ end
 
 --add buffer space for reset feature
 function sandboxOptionsEnd()
-	eHelicopterSandbox.menu["generalSpaceA"] = nil
-	eHelicopterSandbox.menu["resetEventsToolTip"] = nil
-	eHelicopterSandbox.menu["resetEvents"] = nil
-	eHelicopterSandbox.menu["generalSpaceD"] = nil
-	eHelicopterSandbox.menu["eventMarkersOnToolTip"] =  nil
-	eHelicopterSandbox.menu["eventMarkersOn"] = nil
-	eHelicopterSandbox.menu["generalSpaceE"] = nil
+	eHelicopterSandbox.menu.generalSpaceA = nil
+	eHelicopterSandbox.menu.resetEventsToolTip = nil
+	eHelicopterSandbox.menu.resetEvents = nil
+	eHelicopterSandbox.menu.generalSpaceD = nil
+	eHelicopterSandbox.menu.eventMarkersOnToolTip =  nil
+	eHelicopterSandbox.menu.eventMarkersOn = nil
+	eHelicopterSandbox.menu.generalSpaceE = nil
 
-	eHelicopterSandbox.menu["generalSpaceA"] = {type = "Space", alwaysAccessible = true}
-	eHelicopterSandbox.menu["resetEventsToolTip"] = {type = "Text", text = "Reset scheduled events in case of emergency:", a=0.65, customX=-67}
-	eHelicopterSandbox.menu["resetEvents"] = {type = "Tickbox", title = "Reset Events", tooltip = "", }
-	eHelicopterSandbox.menu["generalSpaceD"] = {type = "Space"}
-	eHelicopterSandbox.menu["eventMarkersOnToolTip"] = {type = "Text", text = "Toggle this on to enable event markers. \nNote: Events markers can be dragged.", a=0.65, customX=-67, }
-	eHelicopterSandbox.menu["eventMarkersOn"] = { type = "Tickbox", title = "Event Markers", alwaysAccessible = true}
-	eHelicopterSandbox.menu["generalSpaceE"] = {type = "Space", alwaysAccessible = true}
+	eHelicopterSandbox.menu.generalSpaceA = {type = "Space", alwaysAccessible = true}
+	eHelicopterSandbox.menu.resetEventsToolTip = {type = "Text", a=0.65, customX=-67}
+	eHelicopterSandbox.menu.resetEvents = {type = "Tickbox", tooltip = "", }
+	eHelicopterSandbox.menu.generalSpaceD = {type = "Space"}
+	eHelicopterSandbox.menu.eventMarkersOnToolTip1 = {type = "Text", a=0.65, customX=-67, }
+	eHelicopterSandbox.menu.eventMarkersOnToolTip2 = {type = "Text", a=0.65, customX=-67, }
+	eHelicopterSandbox.menu.eventMarkersOn = { type = "Tickbox", alwaysAccessible = true}
+	eHelicopterSandbox.menu.generalSpaceE = {type = "Space", alwaysAccessible = true}
 
 	loadAnnouncersToConfig()
 end
