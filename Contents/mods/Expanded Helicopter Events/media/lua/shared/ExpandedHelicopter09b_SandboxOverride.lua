b2565d5ffb537d6b3f8947973df46160a0cb3d89
Chuckleberry Finn <6071161+ChuckTheSheep@users.noreply.github.com>
2022-07-29 14:21:53 -0400

    Reimplement sandbox override + correct depreciation handler

--- Contents/mods/Expanded Helicopter Events/media/lua/shared/ExpandedHelicopter09b_SandboxOverride.lua
+++ Contents/mods/Expanded Helicopter Events/media/lua/shared/ExpandedHelicopter09b_SandboxOverride.lua
@@ -1,23 +1,21 @@
 --Overrides vanilla helicopter frequency on game boot
 function HelicopterSandboxOptions()
 	print("EHE: Disabling vanilla helicopter. Adding items to WorldItemRemovalList.")
-	getSandboxOptions():getOptionByName("Helicopter"):setValue(1) -- 1 = Never
-	SandboxVars.Helicopter = 1
 
-	getSandboxOptions():getOptionByName("Helicopter")
+	getSandboxOptions():set("Helicopter", 1)
 
 	--appreciate depreciated values, --TODO: Remove this in a while
 	if SandboxVars.ExpandedHeli.CutOffDay ~= 90 and SandboxVars.ExpandedHeli.SchedulerDuration == 90 then
 		print("WARNING: SandboxVars.ExpandedHeli.CutOffDay non-default value found: appreciating SchedulerDuration to match.")
-		SandboxVars.ExpandedHeli.SchedulerDuration = SandboxVars.ExpandedHeli.CutOffDay
+		SandboxVars.ExpandedHeli.SchedulerDuration = tonumber(tostring(SandboxVars.ExpandedHeli.CutOffDay))
 	end
 	if SandboxVars.ExpandedHeli.NeverEnding ~= false and SandboxVars.ExpandedHeli.ContinueScheduling == false then
 		print("WARNING: SandboxVars.ExpandedHeli.NeverEnding non-default value found: appreciating ContinueScheduling to match.")
-		SandboxVars.ExpandedHeli.ContinueScheduling = SandboxVars.ExpandedHeli.NeverEnding
+		SandboxVars.ExpandedHeli.ContinueScheduling = (tostring(SandboxVars.ExpandedHeli.NeverEnding)=="true")
 	end
 	if SandboxVars.ExpandedHeli.NeverEndingLateGameOnly ~= true and SandboxVars.ExpandedHeli.ContinueSchedulingLateGameOnly == true then
 		print("WARNING: SandboxVars.ExpandedHeli.NeverEndingLateGameOnly non-default value found: appreciating ContinueSchedulingLateGameOnly to match.")
-		SandboxVars.ExpandedHeli.ContinueSchedulingLateGameOnly = SandboxVars.ExpandedHeli.NeverEndingLateGameOnly
+		SandboxVars.ExpandedHeli.ContinueSchedulingLateGameOnly = (tostring(SandboxVars.ExpandedHeli.NeverEndingLateGameOnly)=="true")
 	end
 
 	local typesForRemovalList = {"EHE.EvacuationFlyer","EHE.EmergencyFlyer","EHE.QuarantineFlyer","EHE.PreventionFlyer","EHE.NoticeFlyer"}
@@ -26,9 +24,17 @@ function HelicopterSandboxOptions()
 			SandboxVars.WorldItemRemovalList = SandboxVars.WorldItemRemovalList..","..type
 		end
 	end
-	--getSandboxOptions():updateFromLua()
+
+	if isServer() then
+		local serverFileName = getServerName()
+		print("serverFileName: "..serverFileName)
+		if serverFileName then
+			getSandboxOptions():saveServerLuaFile(serverFileName)
+		end
+	end
+
 end
 
-
+Events.OnGameStart.Add(HelicopterSandboxOptions)
 Events.OnGameBoot.Add(HelicopterSandboxOptions)
 Events.OnLoad.Add(HelicopterSandboxOptions)
