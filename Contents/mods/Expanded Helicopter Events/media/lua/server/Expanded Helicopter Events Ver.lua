ba7f74054b8fcb589b4dc9ad64379fe9ccc96e1c
Chuckleberry Finn <6071161+ChuckTheSheep@users.noreply.github.com>
2022-08-31 16:36:42 -0400

    Update Expanded Helicopter Events Ver.lua

--- Contents/mods/Expanded Helicopter Events/media/lua/server/Expanded Helicopter Events Ver.lua
+++ Contents/mods/Expanded Helicopter Events/media/lua/server/Expanded Helicopter Events Ver.lua
@@ -1,1 +1,1 @@
-Events.OnGameBoot.Add(function () print("Expanded Helicopter Events: ver:0.9-AUG-30-22") end)
+Events.OnGameBoot.Add(function () print("Expanded Helicopter Events: ver:0.9.0.1-HOTFIX-AUG-31-22") end)
