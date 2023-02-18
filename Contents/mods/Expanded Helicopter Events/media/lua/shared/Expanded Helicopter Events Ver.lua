a3e6e706bb3dc264f73fa247d1425de8067e2711
Chuckleberry Finn <6071161+ChuckTheSheep@users.noreply.github.com>
2023-02-18 10:54:53 -0500

    Refactor SandboxOverride to fire once on load/boot(MP) + debugInfoDump

--- Contents/mods/Expanded Helicopter Events/media/lua/shared/Expanded Helicopter Events Ver.lua
+++ Contents/mods/Expanded Helicopter Events/media/lua/shared/Expanded Helicopter Events Ver.lua
@@ -1,15 +1,2 @@
-local function printVersionInfo()
-
-    local text = "Expanded Helicopter Events: ver:0.9.2 FEB_04_23"
-
-    local gameTime = getGameTime()
-    local heliValue = SandboxVars.Helicopter
-    local day, startHr, endHr = gameTime:getHelicopterDay(), gameTime:getHelicopterStartHour(), gameTime:getHelicopterEndHour()
-    text = text.."\n\n    DEBUG: Vanilla Helicopter: ("..heliValue..")  day:"..day.." startHr:"..startHr.." endHr"..endHr
-
-    text = text.."\n\n    EHE-Sandbox Options:"
-    for k,v in pairs(SandboxVars.ExpandedHeli) do text = text.."\n       "..tostring(k).." = "..tostring(v) end
-
-    print(text.."\n")
-end
+local function printVersionInfo() print("Expanded Helicopter Events: ver:0.9.2 FEB_04_23") end
 Events.OnGameBoot.Add(printVersionInfo)
--- Contents/mods/Expanded Helicopter Events/media/lua/shared/ExpandedHelicopter09b_SandboxOverride.lua
+++ Contents/mods/Expanded Helicopter Events/media/lua/shared/ExpandedHelicopter09b_SandboxOverride.lua
@@ -1,9 +1,13 @@
 --Overrides vanilla helicopter frequency on game boot
-local function HelicopterSandboxOptions()
-	print("EHE: Disabling vanilla helicopter. Adding items to WorldItemRemovalList.")
-
-	--getSandboxOptions():set("Helicopter", 1)
+local function HelicopterSandboxOptions(EVENT)
+	EVENT = EVENT or "ERROR-NoEventID"
+	local text = "EHE: ("..EVENT..")  Disabling vanilla helicopter. Adding items to WorldItemRemovalList."
 	SandboxVars.Helicopter = 1
+
+	local gameTime = getGameTime()
+	gameTime:setHelicopterDay(999999999)
+	gameTime:setHelicopterStartHour(0)
+	gameTime:setHelicopterEndHour(0)
 
 	local typesForRemovalList = {"EHE.EvacuationFlyer","EHE.EmergencyFlyer","EHE.QuarantineFlyer","EHE.PreventionFlyer","EHE.NoticeFlyer"}
 	for k,type in pairs(typesForRemovalList) do
@@ -12,15 +16,22 @@ local function HelicopterSandboxOptions()
 		end
 	end
 
-	--if isServer() then
-	--	local serverFileName = getServerName()
-	--	print("serverFileName: "..serverFileName)
-	--	if serverFileName then
-	--		getSandboxOptions():saveServerLuaFile(serverFileName)
-	--	end
-	--end
+	local core = getCore()
+	text = text.."\n    GAME-MODE: "..(isIngameState() and "Main-Menu" or core:getGameMode()).."  "..core:getVersion()..(getDebug() and " (DEBUG)" or "")
+
+
+	local heliValue = SandboxVars.Helicopter
+	local day, startHr, endHr = gameTime:getHelicopterDay(), gameTime:getHelicopterStartHour(), gameTime:getHelicopterEndHour()
+
+	if getDebug() then text = text.."\n    DEBUG: Vanilla Helicopter: ("..heliValue..")  day:"..day.." startHr:"..startHr.." endHr"..endHr end
+
+	text = text.."\n    Current Day:"..gameTime:getNightsSurvived().." Hour:"..gameTime:getHour()
+	text = text.."\n    EHE-Sandbox Options:"
+	for k,v in pairs(SandboxVars.ExpandedHeli) do text = text.."\n       "..tostring(k).." = "..tostring(v) end
+
+	print(text.."\n")
 end
 
-Events.OnGameStart.Add(HelicopterSandboxOptions)
-Events.OnGameBoot.Add(HelicopterSandboxOptions)
-Events.OnLoad.Add(HelicopterSandboxOptions)
+--Events.OnGameStart.Add(function() HelicopterSandboxOptions("OnGameStart") end)
+Events.OnLoad.Add(function() HelicopterSandboxOptions("OnLoad") end)
+if isServer() then Events.OnGameBoot.Add(function() HelicopterSandboxOptions("OnGameBoot") end) end
