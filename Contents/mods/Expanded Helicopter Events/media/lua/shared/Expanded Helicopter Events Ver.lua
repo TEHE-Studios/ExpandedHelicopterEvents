c0ac31be5538b6c42e82d2c70968e3abd9753620
Chuckleberry Finn <6071161+ChuckTheSheep@users.noreply.github.com>
2023-03-23 22:21:13 -0400

    Update Expanded Helicopter Events Ver.lua

--- Contents/mods/Expanded Helicopter Events/media/lua/shared/Expanded Helicopter Events Ver.lua
+++ Contents/mods/Expanded Helicopter Events/media/lua/shared/Expanded Helicopter Events Ver.lua
@@ -1,2 +1,2 @@
-local function printVersionInfo() print("Expanded Helicopter Events: ver:0.9.2 FEB_04_23") end
+local function printVersionInfo() print("Expanded Helicopter Events: ver:0.9.3 MAR_23_HOTFIX_STOPALL") end
 Events.OnGameBoot.Add(printVersionInfo)
