0515dd8f3cbaffad0177b9e97bf8a3ebcb964b3f
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2023-08-15 18:17:25 -0400

    Add sandbox option for weather impact
    Fixes #156

--- Contents/mods/Expanded Helicopter Events/media/lua/shared/ExpandedHelicopter00f_WeatherImpact.lua
+++ Contents/mods/Expanded Helicopter Events/media/lua/shared/ExpandedHelicopter00f_WeatherImpact.lua
@@ -12,11 +12,11 @@ function eHeliEvent_weatherImpact()
 	local snow = CM:getSnowIntensity()/2
 	local thunder = CM:getIsThunderStorming()
 
-	if (wind+rain+snow > 1.1) or (fog > 0.33) or (thunder == true) then
-		willFly = false
-	end
+	if (wind+rain+snow > 1.1) or (fog > 0.33) or (thunder == true) then willFly = false end
 
 	impactOnFlightSafety = math.floor(((wind+rain+snow+fog)/6)+0.5)
+
+	if SandboxVars.ExpandedHeli.WeatherImpactsEvents == false then return true, 0 end
 
 	return willFly, impactOnFlightSafety
 end
