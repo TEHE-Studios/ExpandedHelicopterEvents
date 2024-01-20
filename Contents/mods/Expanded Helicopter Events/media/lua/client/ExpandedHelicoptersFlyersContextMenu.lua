e9113177950b0659e633fb9c014a854bb5d9337d
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2024-01-20 00:09:56 -0500

    Update ExpandedHelicoptersFlyersContextMenu.lua

--- Contents/mods/Expanded Helicopter Events/media/lua/client/ExpandedHelicoptersFlyersContextMenu.lua
+++ Contents/mods/Expanded Helicopter Events/media/lua/client/ExpandedHelicoptersFlyersContextMenu.lua
@@ -1,26 +1,41 @@
---- Chuckleberry Finn
+local paperContext = {}
 
-require "ISUI/ISInventoryPaneContextMenu"
+paperContext._registeredTypes = {"EHE.EvacuationFlyer", "EHE.PreventionFlyer", "EHE.EmergencyFlyer", "EHE.QuarantineFlyer", "EHE.NoticeFlyer"}
 
-local clothingInventoryContainerContextFix = {}
-function clothingInventoryContainerContextFix.apply(player, context, items)
+paperContext.registeredTypes = {}
+for _,iType in pairs(paperContext._registeredTypes) do paperContext.registeredTypes[iType] = true end
 
-    local testItem, clothing
+function paperContext.registerType(iType) paperContext.registeredTypes[iType] = true end
 
-    for i,v in ipairs(items) do
-        testItem = v
-        if not instanceof(v, "InventoryItem") then testItem = v.items[1] end
+---@param context ISContextMenu
+function paperContext.addInventoryItemContext(playerID, context, items)
+    local playerObj = getSpecificPlayer(playerID)
 
-        if instanceof(testItem, "InventoryContainer") and testItem:canBeEquipped() ~= nil and testItem:canBeEquipped() ~= "" and not testItem:isEquipped() then
-            clothing = testItem
+    for _, v in ipairs(items) do
+
+        ---@type InventoryItem
+        local item = v
+        local stack
+        if not instanceof(v, "InventoryItem") then
+            stack = v
+            item = v.items[1]
         end
-    end
 
-    if clothing and
-            not context:getOptionFromName(getText("ContextMenu_Wear")) and
-            not context:getOptionFromName(getText("ContextMenu_Equip_on_your_Back")) then
-        ISInventoryPaneContextMenu.doWearClothingMenu(player, clothing, items, context)
+        local isFlyer = paperContext.registeredTypes[item:getFullType()]
+        if isFlyer then
+
+            local readOption = context:getOptionFromName(getText("ContextMenu_CheckMap"))
+            readOption.name = getText("ContextMenu_Read")
+            --context:addOption(getText("ContextMenu_CheckMap"), map, ISInventoryPaneContextMenu.onCheckMap, player);
+
+            local renameOption = context:getOptionFromName(getText("ContextMenu_RenameMap"))
+            renameOption.name = getText("ContextMenu_RenameBag")
+            --context:addOption(getText("ContextMenu_RenameMap"), map, ISInventoryPaneContextMenu.onRenameMap, player);
+        end
+        break
     end
 end
 
-Events.OnFillInventoryObjectContextMenu.Add(clothingInventoryContainerContextFix.apply)
+Events.OnFillInventoryObjectContextMenu.Add(paperContext.addInventoryItemContext)
+
+return paperContext
