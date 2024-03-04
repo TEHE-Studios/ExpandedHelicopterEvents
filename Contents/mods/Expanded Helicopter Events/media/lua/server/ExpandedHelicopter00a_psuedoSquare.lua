cfb3ac208d20b4b6fe9cfd69611af37d04f8a834
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2024-03-03 19:31:55 -0500

    Create ExpandedHelicopter00a_psuedoSquare.lua

---
+++ Contents/mods/Expanded Helicopter Events/media/lua/server/ExpandedHelicopter00a_psuedoSquare.lua
@@ -0,0 +1,23 @@
+local pseudoSquare = {}
+
+pseudoSquare.x = 0
+pseudoSquare.y = 0
+pseudoSquare.z = 0
+
+function pseudoSquare:new(x, y, z)
+    local newSquare = copyTable(self)
+    newSquare.x = x
+    newSquare.y = y
+    newSquare.z = z
+    return newSquare
+end
+
+function pseudoSquare:getX() return self.x end
+
+function pseudoSquare:getY() return self.y end
+
+function pseudoSquare:getZ() return self.z end
+
+function pseudoSquare:getSquare() return getSquare(self.x, self.y, self.z) end
+
+return pseudoSquare
