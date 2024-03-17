ea3b9abf73479543dbae09fc969da39a63b100f1
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2024-03-17 19:10:54 -0400

    Update babyGate.lua

--- Contents/mods/Expanded Helicopter Events/media/lua/shared/babyGate.lua
+++ Contents/mods/Expanded Helicopter Events/media/lua/shared/babyGate.lua
@@ -1,4 +1,6 @@
+--[[
 if not isClient() then
     local _ll = getFileReader("babyGate.lua", false)
     if not _ll then if isClient() then getCore():quitToDesktop() else local m, lCF = nil, getCoroutineCallframeStack(getCurrentCoroutine(),0) local fD = lCF ~= nil and lCF and getFilenameOfCallframe(lCF) m = fD and getModInfo(fD:match("(.-)media/")) toggleModActive(m, false) end return end
 end
+--]]
