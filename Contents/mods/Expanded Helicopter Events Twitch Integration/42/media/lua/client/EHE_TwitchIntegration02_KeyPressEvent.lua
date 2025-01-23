240bcc25e0b3a526f3c69affad22f76d1323a47f
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2025-01-22 20:01:04 -0500

    fix keyboard issue with mouse

--- Contents/mods/Expanded Helicopter Events Twitch Integration/42/media/lua/client/EHE_TwitchIntegration02_KeyPressEvent.lua
+++ Contents/mods/Expanded Helicopter Events Twitch Integration/42/media/lua/client/EHE_TwitchIntegration02_KeyPressEvent.lua
@@ -1,8 +1,9 @@
 require "ExpandedHelicopter00a_Util"
 
-local twitchKeys = {["KP_1"]="Numpad1",["KP_2"]="Numpad2",["KP_3"]="Numpad3",
-			  ["KP_4"]="Numpad4",["KP_5"]="Numpad5",["KP_6"]="Numpad6",
-			  ["KP_7"]="Numpad7",["KP_8"]="Numpad8",["KP_9"]="Numpad9",}
+local twitchKeys = {
+	["KP_1"]="Numpad1",["KP_2"]="Numpad2",["KP_3"]="Numpad3",
+	["KP_4"]="Numpad4",["KP_5"]="Numpad5",["KP_6"]="Numpad6",
+	["KP_7"]="Numpad7",["KP_8"]="Numpad8",["KP_9"]="Numpad9",}
 
 function twitchIntegration_OnKeyPressed(key)
 
@@ -12,17 +13,15 @@ function twitchIntegration_OnKeyPressed(key)
 		end
 	end
 
-	local twitchKey = twitchKeys[Keyboard.getKeyName(key)]
+	local twitchKey = twitchKeys[getKeyName(key)]
 	if twitchKey then
-
 		local players = getActualPlayers()
 		---@type IsoGameCharacter|IsoPlayer|IsoMovingObject|IsoObject
 		local playerChar = players[ZombRand(#players)+1]
-
 		if eHelicopterSandbox.config.twitchStreamerTargeted == true then playerChar = getPlayer() end
 		if playerChar then
 			local presetConfigNum = eHelicopterSandbox.config[twitchKey]
-			print("-- scheduleEvent: twitchKey: "..tostring(twitchKey))
+			--print("-- scheduleEvent: twitchKey: "..tostring(twitchKey))
 			local pUsername = playerChar:getUsername()
 			sendClientCommand("twitchIntegration", "scheduleEvent", {twitchKey=twitchKey,presetConfigNum=presetConfigNum,twitchTarget=pUsername})
 		end
