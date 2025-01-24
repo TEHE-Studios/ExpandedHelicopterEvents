56e4998c0a91db0940fef81a8f0925ee818aa2ad
sharkster91 <79799951+sharkster91@users.noreply.github.com>
2025-01-24 13:58:09 -0500

    Update ExpandedHelicopter_HairOutfitDefinitions.lua

--- Contents/mods/Expanded Helicopter Events/42/media/lua/server/Definitions/ExpandedHelicopter_HairOutfitDefinitions.lua
+++ Contents/mods/Expanded Helicopter Events/42/media/lua/server/Definitions/ExpandedHelicopter_HairOutfitDefinitions.lua
@@ -3,98 +3,13 @@ require 'Definitions/HairOutfitDefinitions'
 --Expanded Helicopter Events--
 
 local cat = {};
-cat.outfit = "EHEMilitaryPilot";
+cat.outfit = "EHE_HelicopterPilot";
 cat.haircut = "Short:34;Bald:33;CrewCut:33;";
 cat.beard = "None:100";
 table.insert(HairOutfitDefinitions.haircutOutfitDefinition, cat);
 
 local cat = {};
-cat.outfit = "EHEPolicePilot";
-cat.haircut = "Bun:20;Kate:20;Ponytail:30";
-table.insert(HairOutfitDefinitions.haircutOutfitDefinition, cat);
-
-local cat = {};
-cat.outfit = "EHEPolicePilot";
+cat.outfit = "EHE_Soldier";
 cat.haircut = "Short:34;Bald:33;CrewCut:33;";
 cat.beard = "None:100";
 table.insert(HairOutfitDefinitions.haircutOutfitDefinition, cat);
-
-local cat = {};
-cat.outfit = "EHEPolicePilot";
-cat.haircut = "Short:34;Bald:33;CrewCut:33;";
-table.insert(HairOutfitDefinitions.haircutOutfitDefinition, cat);
-
-local cat = {};
-cat.outfit = "EHECivilianPilot";
-cat.haircut = "Short:34;Bald:33;CrewCut:33;";
-table.insert(HairOutfitDefinitions.haircutOutfitDefinition, cat);
-
-local cat = {};
-cat.outfit = "EHECivilianPilot";
-cat.haircut = "Bun:20;Kate:20;Ponytail:30";
-table.insert(HairOutfitDefinitions.haircutOutfitDefinition, cat);
-
-local cat = {};
-cat.outfit = "EHECivilianPilot";
-cat.haircut = "Short:34;Bald:33;CrewCut:33;";
-cat.beard = "None:100";
-table.insert(HairOutfitDefinitions.haircutOutfitDefinition, cat);
-
-local cat = {};
-cat.outfit = "EHECivilianPilot";
-cat.haircut = "Bun:20;Kate:20;Ponytail:30";
-table.insert(HairOutfitDefinitions.haircutOutfitDefinition, cat);
-
-local cat = {};
-cat.outfit = "EHESoldier";
-cat.haircut = "Short:34;Bald:33;CrewCut:33;";
-cat.beard = "None:100";
-table.insert(HairOutfitDefinitions.haircutOutfitDefinition, cat);
-
-local cat = {};
-cat.outfit = "EHESoldier";
-cat.haircut = "Bun:20;Kate:20;Ponytail:30";
-table.insert(HairOutfitDefinitions.haircutOutfitDefinition, cat);
-
-local cat = {};
-cat.outfit = "EHEPoliceOfficer";
-cat.haircut = "Short:34;Bald:33;CrewCut:33;";
-cat.beard = "None:100";
-table.insert(HairOutfitDefinitions.haircutOutfitDefinition, cat);
-
-local cat = {};
-cat.outfit = "EHEPoliceOfficer";
-cat.haircut = "Bun:20;Kate:20;Ponytail:30";
-table.insert(HairOutfitDefinitions.haircutOutfitDefinition, cat);
-
-local cat = {};
-cat.outfit = "EHESamaritanPilot";
-cat.haircut = "Short:34;Bald:33;CrewCut:33;";
-table.insert(HairOutfitDefinitions.haircutOutfitDefinition, cat);
-
-local cat = {};
-cat.outfit = "EHENightwatchPilot";
-cat.haircut = "Short:34;Bald:33;CrewCut:33;";
-cat.beard = "None:100";
-table.insert(HairOutfitDefinitions.haircutOutfitDefinition, cat);
-
-local cat = {};
-cat.outfit = "EHERaider";
-cat.haircut = "Short:34;Bald:33;CrewCut:33;";
-cat.beard = "None:100";
-table.insert(HairOutfitDefinitions.haircutOutfitDefinition, cat);
-
-local cat = {};
-cat.outfit = "EHERaiderLeader";
-cat.haircut = "Short:34;Bald:33;CrewCut:33;";
-cat.beard = "None:100";
-table.insert(HairOutfitDefinitions.haircutOutfitDefinition, cat);
-
-local cat = {};
-cat.outfit = "EHEBlackPilot";
-cat.haircut = "Short:34;Bald:33;CrewCut:33;";
-cat.beard = "None:100";
-table.insert(HairOutfitDefinitions.haircutOutfitDefinition, cat);
-
-
-
