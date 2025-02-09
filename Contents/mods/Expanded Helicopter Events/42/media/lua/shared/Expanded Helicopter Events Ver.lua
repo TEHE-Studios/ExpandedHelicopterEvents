50bfb11940a24b040f68407d252fc227fca9fe15
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2025-02-09 11:18:36 -0500

    Update Expanded Helicopter Events Ver.lua

--- Contents/mods/Expanded Helicopter Events/42/media/lua/shared/Expanded Helicopter Events Ver.lua
+++ Contents/mods/Expanded Helicopter Events/42/media/lua/shared/Expanded Helicopter Events Ver.lua
@@ -1,2 +1,2 @@
-local function printVersionInfo() print("Expanded Helicopter Events: JULY 16_PRE_B42_DUST_OFF") end
+local function printVersionInfo() print("Expanded Helicopter Events: FEB_B42_2025") end
 Events.OnGameBoot.Add(printVersionInfo)
