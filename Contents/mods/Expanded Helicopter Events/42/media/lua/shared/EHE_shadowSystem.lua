41faf99638b81e21e56139108e7d4b28913fa3ad
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2025-06-25 23:23:19 -0400

    test

--- Contents/mods/Expanded Helicopter Events/42/media/lua/shared/EHE_shadowSystem.lua
+++ Contents/mods/Expanded Helicopter Events/42/media/lua/shared/EHE_shadowSystem.lua
@@ -4,48 +4,40 @@ eventShadowHandler = {}
 storedShadows = {}
 storedShadowsUpdateTimes = {}
 --eventShadowHandler:setShadowPos(self.ID, self.shadowTexture, currentSquare:getX(),currentSquare:getY(),currentSquare:getX(),currentSquare:getZ())
+
 function eventShadowHandler:setShadowPos(ID, texture, x, y, z)
 	if isServer() then
 		sendServerCommand("eventShadowHandler", "setShadowPos", {ID=ID,texture=texture,x=x,y=y,z=z})
-	else
-		--print("set Shadow Pos: "..ID.." - "..tostring(texture).." - "..tostring(x)..","..tostring(y)..","..tostring(z))
-		if not ID or not x or not y then
-			return
-		end
-
-		local square
-		if x and y then
-			square = getSquare(x, y, 0)
-		end
-		local outsideSquare
-		if square then
-			outsideSquare = getOutsideSquareFromAbove(square) or square
-		end
-
-		if not texture or not x or not y or not z then outsideSquare = nil end
-
-		---@type WorldMarkers.GridSquareMarker
-		local shadow = storedShadows["HELI"..ID]
-
-		if not shadow and outsideSquare then
-			--print("-- no shadow but yes square")
-			shadow = getWorldMarkers():addGridSquareMarker(texture, nil, outsideSquare, 0.2, 0.2, 0.2, true, 1, 0, 0.25, 0.25)
-		end
-
-		if shadow then
-
-			storedShadowsUpdateTimes["HELI"..ID] = getTimeInMillis()
-
-			--print("-- -- yes shadow, square:?")
-			if not outsideSquare then
-				--print("-- -- -- no square : hide shadow")
-				shadow:setAlpha(0)
-			else
-				--print("-- -- -- yes square : show")
-				shadow:setAlpha(0.25)
-				shadow:setPosAndSize(outsideSquare:getX(),outsideSquare:getY(),outsideSquare:getZ(), 5)
-			end
-			storedShadows["HELI"..ID] = shadow
-		end
+		return
 	end
+
+	if not ID or not x or not y or not z or not texture then return end
+
+	if not getSquare(x, y, 0) then return end
+
+	local shadow = storedShadows["HELI"..ID]
+	if not shadow then
+		storedShadows["HELI"..ID] = getTexture("media/textures/highlights/"..texture)
+		shadow = storedShadows["HELI"..ID]
+	end
+
+	storedShadowsUpdateTimes["HELI"..ID] = getTimeInMillis()
+
+	local w, h = 10, 10
+	local halfW, halfH = w / 2, h / 2
+
+	local x1, y1 = x - halfW, y - halfH
+	local x2, y2 = x + halfW, y - halfH
+	local x3, y3 = x + halfW, y + halfH
+	local x4, y4 = x - halfW, y + halfH
+
+	local sx1, sy1 = ISCoordConversion.ToScreen(x1, y1, z)
+	local sx2, sy2 = ISCoordConversion.ToScreen(x2, y2, z)
+	local sx3, sy3 = ISCoordConversion.ToScreen(x3, y3, z)
+	local sx4, sy4 = ISCoordConversion.ToScreen(x4, y4, z)
+
+	print("screen:",getPlayerScreenWidth(0))
+	print("-cords:",sx1,",", sy1,",", sx2,",", sy2,",", sx3,",", sy3,",", sx4,",", sy4)
+
+	getRenderer():render(shadow, sx1, sy1, sx2, sy2, sx3, sy3, sx4, sy4, 1.0, 1.0, 1.0, 0.8, nil)
 end
