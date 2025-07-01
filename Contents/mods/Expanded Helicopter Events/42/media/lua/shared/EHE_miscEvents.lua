adeafc8a464d3879d8a394c4a935fc9610600f99
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2025-07-01 13:59:26 -0400

    Update EHE_miscEvents.lua

--- Contents/mods/Expanded Helicopter Events/42/media/lua/shared/EHE_miscEvents.lua
+++ Contents/mods/Expanded Helicopter Events/42/media/lua/shared/EHE_miscEvents.lua
@@ -25,7 +25,7 @@ function eHelicopter_dropCrewOff(heli)
 	x = x+xOffset
 	y = y+yOffset
 
-	--[[DEBUG]] print("SWH: DEBUG: eHelicopter_dropCrewOff: "..x..","..y)
+	--[[DEBUG]] print("EHE: DEBUG: eHelicopter_dropCrewOff: "..x..","..y)
 	--for k,v in pairs(heli.crew) do print(" -- k:"..tostring(k).." -- ("..tostring(v)..")") end
 
 	eventMarkerHandler.setOrUpdate(getRandomUUID(), "media/ui/crew.png", 750, x, y, heli.markerColor)
@@ -67,45 +67,3 @@ function eHelicopter_crewSeek(crew)
 		end
 	end
 end
-
-
-local activeMods = {}
-local activeModIDs = getActivatedMods()
-for i=1, activeModIDs:size() do
-	local modID = activeModIDs:get(i-1)
-	if not activeMods[modID] then
-		activeMods[modID] = true
-	end
-end
-
-
-superWeirdForcedDancersAnim = {}
-local OrdinaryDance = {
-	"BobTA_African_Noodle", "BobTA_African_Rainbow", "BobTA_Arm_Push", "BobTA_Arm_Wave_One", "BobTA_Arm_Wave_Two",
-	"BobTA_Arms_Hip_Hop", "BobTA_Around_The_World", "BobTA_Bboy_Hip_Hop_One", "BobTA_Bboy_Hip_Hop_Three", "BobTA_Bboy_Hip_Hop_Two",
-	"BobTA_Body_Wave", "BobTA_Booty_Step", "BobTA_Breakdance_Brooklyn_Uprock", "BobTA_Cabbage_Patch", "BobTA_Can_Can",
-	"BobTA_Charleston", "BobTA_Chicken", "BobTA_Crazy_Legs", "BobTA_Defile_De_Samba_Parade", "BobTA_Gandy", "BobTA_Hokey_Pokey",
-	"BobTA_House_Dancing", "BobTA_Kick_Step", "BobTA_Locking", "BobTA_Macarena", "BobTA_Maraschino", "BobTA_MoonWalk_One",
-	"BobTA_Moonwalk_Two", "BobTA_Northern_Soul_Spin", "BobTA_Northern_Soul_Spin_On_Floor", "BobTA_Raise_The_Roof",
-	"BobTA_Really_Twirl", "BobTA_Rib_Pops", "BobTA_Rockette_Kick", "BobTA_Rumba_Dancing", "BobTA_Running_Man_One",
-	"BobTA_Running_Man_Three", "BobTA_Running_Man_Two", "BobTA_Salsa", "BobTA_Salsa_Double_Twirl", "BobTA_Salsa_Double_Twirl_and_Clap",
-	"BobTA_Salsa_Side_to_Side", "BobTA_Shim_Sham", "BobTA_Shimmy", "BobTA_Shuffling", "BobTA_Side_to_Side", "BobTA_Thriller_One",
-	"BobTA_Twist_One", "BobTA_Twist_Two", "BobTA_Uprock_Indian_Step", "BobTA_YMCA",}
----@param char IsoGameCharacter
-function forceDance(heli, char)
-	if not activeMods["TrueActionsDancing"] then return end
-
-	if instanceof(char, "IsoPlayer") then
-		local currentEmote = char:getVariableString("emote")
-		if (not superWeirdForcedDancersAnim[char]~=currentEmote) then
-			local dance = superWeirdForcedDancersAnim[char] or OrdinaryDance[ZombRand(#OrdinaryDance)+1]
-			local danceRecipe = string.gsub(dance, "_", " ")
-			if not char:isRecipeKnown(danceRecipe) then
-				char:getKnownRecipes():add(danceRecipe)
-			end
-			char:playEmote(dance)
-			superWeirdForcedDancersAnim[char] = dance
-		end
-	end
-end
-function onLaunchClearDance() superWeirdForcedDancersAnim = {} end
