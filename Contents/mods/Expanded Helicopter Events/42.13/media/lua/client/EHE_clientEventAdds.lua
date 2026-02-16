83bb0b504d7f158faeb9d5d1469bfd1197635f69
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2026-02-16 08:44:56 -0500

    Update EHE_clientEventAdds.lua

--- Contents/mods/Expanded Helicopter Events/42.13/media/lua/client/EHE_clientEventAdds.lua
+++ Contents/mods/Expanded Helicopter Events/42.13/media/lua/client/EHE_clientEventAdds.lua
@@ -1,3 +1,12 @@
 local paperContext = require "EHE_flyersContextMenu.lua"
 Events.OnFillInventoryObjectContextMenu.Add(paperContext.addInventoryItemContext)
 
+--[[
+---Leaving this for later
+local errorMagnifier = require "errorMagnifier_Main"
+if errorMagnifier and errorMagnifier.registerDebugReport then
+    errorMagnifier.registerDebugReport("ExpandedHelicopterEvents", function()
+        return { }
+    end, "Expanded Helicopter Events")
+end
+--]]
