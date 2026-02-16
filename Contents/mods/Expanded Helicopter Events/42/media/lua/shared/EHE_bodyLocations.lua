95c4984a5406fdb702d9878fb6f486df5597244e
sharkster91 <79799951+sharkster91@users.noreply.github.com>
2026-02-16 00:40:48 -0500

    Delete EHE_bodyLocations.lua

--- Contents/mods/Expanded Helicopter Events/42/media/lua/shared/EHE_bodyLocations.lua
+++
@@ -1,6 +0,0 @@
-require 'NPCs/BodyLocations'
--- Locations must be declared in render-order.
--- Location IDs must match BodyLocation= and CanBeEquipped= values in items.txt.
-local group = BodyLocations.getGroup("Human")
-
-group:getOrCreateLocation("EHEPilotVest")
