7643f9aa04d7d1a0968190319a9cedb7c1d80ceb
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2026-02-16 23:42:53 -0500

    Update EHE_version.lua

--- Contents/mods/Expanded Helicopter Events/42.13/media/lua/shared/EHE_version.lua
+++ Contents/mods/Expanded Helicopter Events/42.13/media/lua/shared/EHE_version.lua
@@ -1,2 +1,2 @@
-local function printVersionInfo() print("Expanded Helicopter Events: B42_JUN_2025") end
+local function printVersionInfo() print("Expanded Helicopter Events: B42_FEB_2026") end
 Events.OnGameBoot.Add(printVersionInfo)
