60c58f728bdbebf32afa3214a085945b3a42b628
sharkster91 <79799951+sharkster91@users.noreply.github.com>
2026-02-25 14:18:29 -0500

    Cleaned up distributions

--- Contents/mods/Expanded Helicopter Events/42.14/media/lua/server/EHE_distributions.lua
+++ Contents/mods/Expanded Helicopter Events/42.14/media/lua/server/EHE_distributions.lua
@@ -1,11 +1,5 @@
 require 'Items/ProceduralDistributions'
 
-table.insert(ProceduralDistributions.list.ArmyHangarOutfit.items, "EHE.Vest_PilotSurvivalVest");
-table.insert(ProceduralDistributions.list.ArmyHangarOutfit.items, 3);
-
-table.insert(ProceduralDistributions.list.ArmySurplusOutfit.items, "EHE.Vest_PilotSurvivalVest");
-table.insert(ProceduralDistributions.list.ArmySurplusOutfit.items, 3);
-
 table.insert(ProceduralDistributions.list.CrateCamping.items, "EHE.SignalFlare");
 table.insert(ProceduralDistributions.list.CrateCamping.items, 3);
 
