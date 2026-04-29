84288910a2b9592fbcfcdb2c8b8b34574a30a1ee
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2026-04-28 23:41:09 -0400

    establish eHeliEvent_FreqDictionary WIP

--- Contents/mods/Expanded Helicopter Events/42.15/media/lua/server/EHE_eventScheduler.lua
+++ Contents/mods/Expanded Helicopter Events/42.15/media/lua/server/EHE_eventScheduler.lua
@@ -230,6 +230,19 @@ local function eHeliEvent_determineContinuation()
 end
 
 
+function eHeliEvent_FreqDictionary(freqID)
+	local freqIDtoActual = {
+		[1] = 0, -- Never
+		[2] = 2, -- Rare
+		[3] = 4, -- Uncommon
+		[4] = 6, -- Common
+		[5] = 8, -- Frequent
+		[6] = 50, -- Insane
+	}
+	return freqIDtoActual[freqID] or 2
+end
+
+
 function eHeliEvent_ScheduleNew(currentDay, currentHour, freqOverride, noPrint)
 	local gameTime = getGameTime()
 	local globalModData = modData.get()
@@ -277,10 +290,7 @@ function eHeliEvent_ScheduleNew(currentDay, currentHour, freqOverride, noPrint)
 
 	eHeliEvents_setEventsForScheduling()
 
-	if #eventsForScheduling <= 0 then
-		print("[EHE] ScheduleNew: no schedulable events found")
-		return
-	end
+	if #eventsForScheduling <= 0 then print("[EHE] ScheduleNew: no schedulable events found") return end
 
 	for _, presetID in pairs(eventsForScheduling) do
 
@@ -308,12 +318,9 @@ function eHeliEvent_ScheduleNew(currentDay, currentHour, freqOverride, noPrint)
 				end
 			end
 
-			local freq = 3
-			local presetFreq = SandboxVars.ExpandedHeli["Frequency_"..presetID]
-			if presetFreq then
-				freq = presetFreq-1
-				if freq == 5 then freq = 50 end
-			end
+
+			local presetFreqID = SandboxVars.ExpandedHeli["Frequency_"..presetID]
+			local freq = eHeliEvent_FreqDictionary(presetFreqID)
 
 			freq = freqOverride or freq
 
